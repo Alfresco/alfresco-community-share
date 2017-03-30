fda22944f000e6c1745aef2617dc073672eeb394
Cristina Jalba <cristina.jalba@ness.com>
2017-03-30 14:49:59 +0300

    no message

--- alfresco-tas-share-test/src/main/java/org/alfresco/po/share/alfrescoContent/applyingRulesToFolders/EditRulesPage.java
+++ alfresco-tas-share-test/src/main/java/org/alfresco/po/share/alfrescoContent/applyingRulesToFolders/EditRulesPage.java
@@ -167,6 +167,7 @@ public class EditRulesPage extends SiteCommon<EditRulesPage>
     public EditRulesPage clickCreateAndCreateAnotherButton()
     {
         createAndCreateAnotherButton.click();
+        browser.refresh();
         return (EditRulesPage) this.renderedPage();
     }
 
