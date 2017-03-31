403324f209c97062ba1cb32d6c6b63753bb2b4e7
cagache <claudia.agache@ness.com>
2017-03-31 17:04:36 +0300

    try to fix ImagePreviewDashletTests

--- alfresco-tas-share-test/src/test/java/org/alfresco/share/site/siteDashboard/ImagePreviewDashletTests.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/share/site/siteDashboard/ImagePreviewDashletTests.java
@@ -1,13 +1,11 @@
 package org.alfresco.share.site.siteDashboard;
 
 import org.alfresco.common.DataUtil;
-import org.alfresco.po.share.UploadFileDialog;
 import org.alfresco.po.share.alfrescoContent.document.DocumentDetailsPage;
 import org.alfresco.po.share.alfrescoContent.workingWithFilesAndFolders.Download;
 import org.alfresco.po.share.dashlet.ImagePreviewDashlet;
 import org.alfresco.po.share.site.DocumentLibraryPage;
 import org.alfresco.po.share.site.SiteDashboardPage;
-import org.alfresco.po.share.user.UserDashboardPage;
 import org.alfresco.share.ContextAwareWebTest;
 import org.alfresco.testrail.TestRail;
 import org.alfresco.utility.model.TestGroup;
@@ -23,9 +21,6 @@ import static org.testng.Assert.*;
 public class ImagePreviewDashletTests extends ContextAwareWebTest
 {
     @Autowired
-    UserDashboardPage userDashboardPage;
-
-    @Autowired
     SiteDashboardPage siteDashboard;
 
     @Autowired
@@ -35,38 +30,30 @@ public class ImagePreviewDashletTests extends ContextAwareWebTest
     DocumentLibraryPage documentLibraryPage;
 
     @Autowired
-    UploadFileDialog uploadDialog;
-
-    @Autowired
     DocumentDetailsPage documentDetailsPage;
 
     @Autowired
     Download download;
 
-    private String siteName1, siteName2, siteName3;
-    private final String uniqueId = DataUtil.getUniqueIdentifier();
+    private String siteName1 = "Site1-" + DataUtil.getUniqueIdentifier();
+    private String siteName2 = "Site2-" + DataUtil.getUniqueIdentifier();
+    private String siteName3 = "Site3-" + DataUtil.getUniqueIdentifier();
+    private final String fileName = "newavatar.jpg";
 
     @BeforeClass(alwaysRun = true)
     public void setupTest()
     {
-        String userName = "User" + uniqueId;
-        String firstName = "firstName";
-        String description = "Description";
-        siteName1 = "Site1-" + uniqueId;
-        siteName2 = "Site2-" + uniqueId;
-        siteName3 = "Site3-" + uniqueId;
-
-        userService.create(adminUser, adminPassword, userName, password, userName + domain, firstName, userName);
-        siteService.create(userName, password, domain, siteName1, description, PUBLIC);
-        siteService.create(userName, password, domain, siteName2, description, PUBLIC);
-        siteService.create(userName, password, domain, siteName3, description, PUBLIC);
+        String userName = "User" + DataUtil.getUniqueIdentifier();
+        userService.create(adminUser, adminPassword, userName, password, userName + domain, userName, userName);
+        siteService.create(userName, password, domain, siteName3, siteName3, PUBLIC);
+        contentService.uploadFileInSite(userName, password, siteName3, testDataFolder + fileName);
+        siteService.create(userName, password, domain, siteName1, siteName1, PUBLIC);
+        siteService.create(userName, password, domain, siteName2, siteName2, PUBLIC);
         siteService.addDashlet(userName, password, siteName1, IMAGE_PREVIEW, TWO_COLUMNS_WIDE_RIGHT, 1, 1);
         siteService.addDashlet(userName, password, siteName2, IMAGE_PREVIEW, TWO_COLUMNS_WIDE_RIGHT, 1, 1);
         siteService.addDashlet(userName, password, siteName3, IMAGE_PREVIEW, TWO_COLUMNS_WIDE_RIGHT, 1, 1);
-        contentService.uploadFileInSite(userName, password, siteName3, testDataFolder + "newavatar.jpg");
 
         setupAuthenticatedSession(userName, password);
-        assertEquals(userDashboardPage.getPageTitle(), "Alfresco » User Dashboard", "Displayed page=");
     }
 
     @TestRail(id = "C5414")
@@ -75,7 +62,7 @@ public class ImagePreviewDashletTests extends ContextAwareWebTest
     {
         LOG.info("STEP 1 - View \"Image Preview\" dashlet");
         siteDashboard.navigate(siteName1);
-        assertEquals(userDashboardPage.getPageTitle(), "Alfresco » Site Dashboard", "Displayed page=");
+        assertEquals(siteDashboard.getPageTitle(), "Alfresco » Site Dashboard", "Displayed page=");
         assertEquals(imagePreviewDashlet.getDashletTitleText(), "Image Preview");
         assertTrue(imagePreviewDashlet.isHelpIconDisplayed(), "Help icon is displayed.");
 
@@ -98,7 +85,7 @@ public class ImagePreviewDashletTests extends ContextAwareWebTest
         assertEquals(documentLibraryPage.getPageTitle(), "Alfresco » Document Library", "Displayed page=");
 
         LOG.info("STEP 2 - Upload some images in \"Document Library\" of the site");
-        documentLibraryPage.uploadNewImage(testDataFolder + "newavatar.jpg");
+        documentLibraryPage.uploadNewImage(testDataFolder + fileName);
         documentLibraryPage.uploadNewImage(testDataFolder + "newavatar.bmp");
         documentLibraryPage.uploadNewImage(testDataFolder + "newavatar.gif");
         documentLibraryPage.uploadNewImage(testDataFolder + "newavatar.png");
@@ -106,8 +93,8 @@ public class ImagePreviewDashletTests extends ContextAwareWebTest
         LOG.info("STEP 3 - Navigate to site's dashboard");
         siteDashboard.navigate(siteName2);
         getBrowser().refresh();
-        assertEquals(userDashboardPage.getPageTitle(), "Alfresco » Site Dashboard", "Displayed page=");
-        assertTrue(imagePreviewDashlet.isImageDisplayed("newavatar.jpg"), ".jpg image displayed");
+        assertEquals(siteDashboard.getPageTitle(), "Alfresco » Site Dashboard", "Displayed page=");
+        assertTrue(imagePreviewDashlet.isImageDisplayed(fileName), ".jpg image displayed");
         assertTrue(imagePreviewDashlet.isImageDisplayed("newavatar.bmp"), ".bmp image displayed");
         assertTrue(imagePreviewDashlet.isImageDisplayed("newavatar.gif"), ".gif image displayed");
         assertTrue(imagePreviewDashlet.isImageDisplayed("newavatar.png"), ".png image displayed");
@@ -117,11 +104,10 @@ public class ImagePreviewDashletTests extends ContextAwareWebTest
     @Test(groups = { TestGroup.SANITY, TestGroup.SITES })
     public void imagePreviewAvailableActions()
     {
-        String fileName = "newavatar.jpg";
-
         LOG.info("Precondition: Navigate to site's dashboard");
         siteDashboard.navigate(siteName3);
-        assertEquals(userDashboardPage.getPageTitle(), "Alfresco » Site Dashboard", "Displayed page=");
+        assertEquals(siteDashboard.getPageTitle(), "Alfresco » Site Dashboard", "Displayed page=");
+        assertTrue(imagePreviewDashlet.isImageDisplayed(fileName), ".jpg image displayed");
 
         LOG.info("Step1: Click 'View Details' icon");
         imagePreviewDashlet.clickViewDetailsIcon(fileName);
@@ -129,7 +115,7 @@ public class ImagePreviewDashletTests extends ContextAwareWebTest
 
         LOG.info("Step2: Navigate to Site Dashboard");
         siteDashboard.navigate(siteName3);
-        assertEquals(userDashboardPage.getPageTitle(), "Alfresco » Site Dashboard", "Displayed page=");
+        assertEquals(siteDashboard.getPageTitle(), "Alfresco » Site Dashboard", "Displayed page=");
 
         LOG.info("Step3: Click 'Download' icon");
         imagePreviewDashlet.clickDownloadIcon(fileName);
