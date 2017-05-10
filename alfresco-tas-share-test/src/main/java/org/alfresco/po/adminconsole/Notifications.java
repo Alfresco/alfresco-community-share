17372b84f45616a1d7dafabcc15b84b0b052ff75
mionescu <madalina.ionescu@ness.com>
2017-05-10 14:45:28 +0300

    add Notification class

---
+++ alfresco-tas-share-test/src/main/java/org/alfresco/po/adminconsole/Notifications.java
@@ -0,0 +1,17 @@
+package org.alfresco.po.adminconsole;
+
+/**
+ * Created by P3700408 on 5/10/2017.
+ */
+public class Notifications {
+
+    /* Tenant Console */
+    public static String CREATE_TENANT = "created tenant: %s";
+    public static String DELETE_TENANT = "Deleted tenant: %s";
+    public static String ENABLED_TENANT = "Enabled tenant: %s";
+    public static String ENABLED_TENANT_RESULT = "Enabled tenant: %s %s";
+    public static String DISABLED_TENANT = "Disabled tenant: %s";
+    public static String DISABLED_TENANT_RESULT = "Disabled tenant: %s %s";
+
+     /* Workflow Console */
+}
