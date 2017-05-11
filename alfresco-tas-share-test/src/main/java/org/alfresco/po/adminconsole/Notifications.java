dbadc9e529a3bfdae6949270088febf32b969cca
mionescu <madalina.ionescu@ness.com>
2017-05-11 09:36:00 +0300

    added new constants

--- alfresco-tas-share-test/src/main/java/org/alfresco/po/adminconsole/Notifications.java
+++ alfresco-tas-share-test/src/main/java/org/alfresco/po/adminconsole/Notifications.java
@@ -9,9 +9,9 @@ public class Notifications {
     public static String CREATE_TENANT = "created tenant: %s";
     public static String DELETE_TENANT = "Deleted tenant: %s";
     public static String ENABLED_TENANT = "Enabled tenant: %s";
-    public static String ENABLED_TENANT_RESULT = "Enabled tenant: %s %s";
+    public static String ENABLED_TENANT_RESULT = "Enabled  - Tenant: %s (%s)";
     public static String DISABLED_TENANT = "Disabled tenant: %s";
-    public static String DISABLED_TENANT_RESULT = "Disabled tenant: %s %s";
+    public static String DISABLED_TENANT_RESULT = "Disabled - Tenant: %s (%s)";
 
      /* Workflow Console */
 }
