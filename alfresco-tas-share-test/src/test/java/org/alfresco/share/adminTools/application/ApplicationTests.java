1d4726d6baa653369b52ef2290305a41e143b724
mtifui <mirela.tifui@ness.com>
2017-06-29 11:03:53 +0300

    fixing applicationTests

--- alfresco-tas-share-test/src/test/java/org/alfresco/share/adminTools/application/ApplicationTests.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/share/adminTools/application/ApplicationTests.java
@@ -13,21 +13,17 @@ import org.testng.annotations.Test;
 /**
  * @author Razvan.Dorobantu
  */
-public class ApplicationTests extends ContextAwareWebTest
-{
-    @Autowired
-    ApplicationPage applicationPage;
+public class ApplicationTests extends ContextAwareWebTest {
+    @Autowired ApplicationPage applicationPage;
 
     @BeforeClass(alwaysRun = true)
-    public void beforeClass()
-    {
+    public void beforeClass() {
         setupAuthenticatedSession(adminUser, adminPassword);
         applicationPage.navigate();
     }
 
     @AfterClass
-    public void afterClass()
-    {
+    public void afterClass() {
         applicationPage.selectTheme(ApplicationPage.Theme.BLUE_THEME);
         if (!applicationPage.isAlfrescoDefaultImageDisplayed())
             applicationPage.resetImageToDefault();
@@ -36,31 +32,24 @@ public class ApplicationTests extends ContextAwareWebTest
 
     @TestRail(id = "C9292")
     @Test(groups = { TestGroup.SANITY, TestGroup.ADMIN_TOOLS })
-    public void addAndResetNewLogo()
-    {
+    public void addAndResetNewLogo() {
         LOG.info("Step 1: Upload a new logo image in the 'Application' page.");
         if (!applicationPage.isAlfrescoDefaultImageDisplayed())
             applicationPage.resetImageToDefault();
         applicationPage.uploadImage();
-
         LOG.info("Step 2: Verify the new logo image was uploaded successfully.");
         Assert.assertFalse(applicationPage.isAlfrescoDefaultImageDisplayed(), "Alfresco default image is not displayed.");
-
         LOG.info("Step 3: Reset the new image to the default one.");
         applicationPage.resetImageToDefault();
-
         LOG.info("Step 4: Verify the new image was removed successfully.");
         Assert.assertTrue(applicationPage.isAlfrescoDefaultImageDisplayed(), "Alfresco default image is displayed.");
     }
 
     @TestRail(id = "C9281")
     @Test(groups = { TestGroup.SANITY, TestGroup.ADMIN_TOOLS })
-    public void changeTheme()
-    {
+    public void changeTheme() {
         LOG.info("Step 1: Select a new theme in the 'Application' page.");
         applicationPage.selectTheme(ApplicationPage.Theme.YELLOW_THEME);
-        applicationPage.renderedPage();
-
         LOG.info("Step 2: Verify the new theme was successfully saved.");
         Assert.assertTrue(applicationPage.isThemeOptionSelected(ApplicationPage.Theme.YELLOW_THEME), "New theme is not selected");
         Assert.assertTrue(applicationPage.doesBodyContainTheme(ApplicationPage.Theme.YELLOW_THEME), "New theme is not in body");
