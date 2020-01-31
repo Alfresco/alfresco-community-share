065f88f68c45677a2fd1d7b49be1a7575335c5a4
iuliaib <iulia.burca@ness.com>
2020-01-31 16:29:21 +0200

    Create PRR-508 Transformation test
    1. Create/Update @PageObject:
    - DocumentTransformationEnginePage - DTE Server page that contains `searchTransformation()` method which is searching for transformed file in History tab -> Transformation Table
    - PerformActionRulePage - contains `transformAndCopy()` method to set the type of transformation and the target and a enum list of `transformAndCopy()` dropdown mimetypes
    - EditRulesPage:
    ** creating methods to define rules for a folder;
    ** creating enum for `When Rule dropdown`;
    ** creating enum for `If All Criteria Are Met Rule`;
    ** creating enum for `Perform Action List`
    
    2. Create @Test class TransformationServerTest that will contain transformation tests
    
    3. Add transformationServerUrl in default.properties file and EnvProperties.java class

--- alfresco-tas-share-test/src/main/java/org/alfresco/po/share/alfrescoContent/applyingRulesToFolders/EditRulesPage.java
+++ alfresco-tas-share-test/src/main/java/org/alfresco/po/share/alfrescoContent/applyingRulesToFolders/EditRulesPage.java
@@ -1,10 +1,7 @@
 package org.alfresco.po.share.alfrescoContent.applyingRulesToFolders;
 
-import static org.testng.Assert.assertEquals;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import org.alfresco.common.Language;
+import org.alfresco.po.share.site.DocumentLibraryPage;
 import org.alfresco.po.share.site.SiteCommon;
 import org.alfresco.utility.web.annotation.PageObject;
 import org.alfresco.utility.web.annotation.RenderWebElement;
@@ -14,12 +11,26 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
 /**
  * @author Laura.Capsa
  */
 @PageObject
 public class EditRulesPage extends SiteCommon<EditRulesPage>
 {
+    @Autowired
+    DocumentLibraryPage documentLibraryPage;
+
+    @Autowired
+    ManageRulesPage manageRulesPage;
+
+    @Autowired
+    protected Language language;
+
     @Autowired
     RuleDetailsPage ruleDetailsPage;
 
@@ -58,6 +69,10 @@ public class EditRulesPage extends SiteCommon<EditRulesPage>
 
     @FindBy (css = "input[id*='ruleConfigUnlessCondition']")
     private WebElement unlessCheckbox;
+
+    private By whenDropDown = By.cssSelector("div[id*=ruleConfigType] .config-name");
+    private By ifAllCriteriaAreMetDropDown = By.cssSelector("div[id*=ruleConfigIfCondition] .config-name");
+    private By performActionDropDown = By.cssSelector("div[id*=ruleConfigAction] .config-name");
 
     private String dropdownSelector = "div[id*='%s'] select[class='config-name']";
 
@@ -262,5 +277,166 @@ public class EditRulesPage extends SiteCommon<EditRulesPage>
     {
         WebElement element = browser.findElement(By.cssSelector(String.format(inputConfigText, textBoxId)));
         return element.getAttribute("value");
+    }
+
+    /**
+     * Select value from `When` dropdown.
+     *
+     * @param whenRuleName  value from WhenRule enum representing the choice from dropdown
+     */
+    public void selectWhenDropDownCondition(WhenRule whenRuleName) {
+        Select whenType = new Select(browser.findElement(whenDropDown));
+        whenType.selectByValue(whenRuleName.getValue());
+    }
+
+    /**
+     * Select value from `If all criteria are met` dropdown.
+     *
+     * @param ifAllCriteriaAreMetRuleName   value from IfAllCriteriaAreMetRule enum representing the condition from dropdown
+     */
+    public void selectIfDropDownCondition(IfAllCriteriaAreMetRule ifAllCriteriaAreMetRuleName) {
+        Select ifType = new Select(browser.findElement(ifAllCriteriaAreMetDropDown));
+        ifType.selectByVisibleText(ifAllCriteriaAreMetRuleName.getName());
+    }
+
+    /**
+     * Select value from `Perform Action` dropdown
+     *
+     * @param performActionName value from PerformActionList enum representing the type of action for the rule from dropdown
+     */
+    public void selectPerformActionDropDown(PerformActionList performActionName) {
+        Select performAction = new Select(browser.findElement(performActionDropDown));
+        performAction.selectByValue(performActionName.getValue());
+    }
+
+    /**
+     * Method that creates a rule for a folder
+     *
+     * @param ruleName                      the given name of the rule
+     * @param siteName                      the site in which is the folder that will have the rule
+     * @param sourceFolder                  the folder that will have the rule
+     * @param whenRuleName                  value of When from WhenRule enum
+     * @param ifAllCriteriaAreMetRuleName   value of If all criteria are met from IfAllCriteriaAreMetRule enum
+     * @param performActionName             value of Perform Action from PerformActionList enum
+     */
+    public void defineRule(String ruleName, String siteName, String sourceFolder, WhenRule whenRuleName, IfAllCriteriaAreMetRule ifAllCriteriaAreMetRuleName, PerformActionList performActionName) {
+        documentLibraryPage.navigate(siteName);
+        documentLibraryPage.clickDocumentLibraryItemAction(sourceFolder, language.translate("documentLibrary.contentActions.manageRules"), manageRulesPage);
+        manageRulesPage.clickCreateRules();
+        typeName(ruleName);
+        selectWhenDropDownCondition(whenRuleName);
+        selectIfDropDownCondition(ifAllCriteriaAreMetRuleName);
+        selectPerformActionDropDown(performActionName);
+    }
+
+    /**
+     * List of values from `When` dropdown
+     */
+    public enum WhenRule {
+        itemsCreatedOrEnterFolder("Items are created or enter this folder", "inbound"),
+        itemsUpdated("Items are updated", "update"),
+        itemsDeletedOrLeaveFolder("Items are deleted or leave this folder", "outbound");
+
+        private String name;
+        private String value;
+
+        WhenRule(String name, String value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+    /**
+     * List of values from `If All Criteria Are Met Rule` dropdown
+     */
+    public enum IfAllCriteriaAreMetRule {
+        ALL_ITEMS("All Items", "no-condition", "condition"),
+        SIZE("Size", "compare-property-value", "SIZE"),
+        CREATED_DATE("Created Date", "compare-property-value", "created"),
+        MODIFIED_DATE("Modified Date", "compare-property-value", "modified"),
+        CREATOR("Creator", "compare-property-value", "creator"),
+        MODIFIER("Modifier", "compare-property-value", "modifier"),
+        AUTHOR("Author", "compare-property-value", "author"),
+        MIMETYPE("Mimetype", "compare-mime-type", "MIME_TYPE"),
+        ENCONDING("Encoding", "compare-property-value", "ENCODING"),
+        DESCRIPTION("Description", "compare-property-value", "description"),
+        NAME("Name", "compare-property-value", "name"),
+        TITLE("Title", "compare-property-value", "title"),
+        HAS_TAG("Has tag", "has-tag", "condition"),
+        HAS_CATEGORY("Has category", "in-category", "condition"),
+        CONTENT_TYPE_SUBTYPE("Content of type or sub-type", "is-subtype", "condition"),
+        HAS_ASPECT("Has aspect", "has-aspect", "condition"),
+        SHOW_MORE("Show more...", "show-more", "item");
+
+        private String name;
+        private String value;
+        private String rel;
+
+        IfAllCriteriaAreMetRule(String name, String value, String rel) {
+            this.name = name;
+            this.value = value;
+            this.rel = rel;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public String getRel() {
+            return rel;
+        }
+    }
+
+    /**
+     * List of values from `Perform Action` dropdown
+     */
+    public enum PerformActionList {
+        SELECT("Select...", "select"),
+        EXECUTE_SCRIPT("Execute script", "script"),
+        COPY("Copy", "copy"),
+        MOVE("Move", "move"),
+        CHECK_IN("Check in", "check-in"),
+        CHECK_OUT("Check out", "check-out"),
+        LINK_TO_CATEGORY("Link to category", "link-category"),
+        ADD_ASPECT("Add aspect", "add-features"),
+        REMOVE_ASPECT("Remove aspect", "remove-features"),
+        ADD_SIMPLE_WORKFLOW("Add simple workflow", "simple-workflow"),
+        SEND_EMAIL("Send email", "mail"),
+        TRANSFORM_AND_COPY_CONTENT("Transform and copy content", "transform"),
+        TRANSFORM_AND_COPY_IMAGE("Transform and copy image", "transform-image"),
+        EXTRACT_COMMON_METADATA_FIELDS("Extract common metadata fields", "extract-metadata"),
+        IMPORT("Import", "import"),
+        SPECIALISE_TYPE("Specialise type", "specialise-type"),
+        INCREMENT_COUNTER("Increment Counter", "counter"),
+        SET_PROPERTY_VALUE("Set property value", "set-property-value"),
+        EMBED_PROPERTY_AS_METADATA_IN_CONTENT("Embed properties as metadata in content", "embed-metadata");
+
+        private String name;
+        private String value;
+
+        PerformActionList(String name, String value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getValue() {
+            return value;
+        }
     }
 }
---
+++ alfresco-tas-share-test/src/test/java/org/alfresco/share/alfrescoContent/applyingRulesToFolders/TransformationServerTest.java
@@ -0,0 +1,88 @@
+package org.alfresco.share.alfrescoContent.applyingRulesToFolders;
+
+import org.alfresco.dataprep.SiteService;
+import org.alfresco.po.DocumentTransformationEnginePage;
+import org.alfresco.po.share.alfrescoContent.applyingRulesToFolders.EditRulesPage;
+import org.alfresco.po.share.alfrescoContent.applyingRulesToFolders.PerformActionRulePage;
+import org.alfresco.po.share.site.DocumentLibraryPage;
+import org.alfresco.share.ContextAwareWebTest;
+import org.alfresco.testrail.TestRail;
+import org.alfresco.utility.data.RandomData;
+import org.alfresco.utility.model.TestGroup;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+
+public class TransformationServerTest extends ContextAwareWebTest {
+
+    @Autowired
+    EditRulesPage editRulesPage;
+
+    @Autowired
+    PerformActionRulePage performActionRulePage;
+
+    @Autowired
+    DocumentLibraryPage documentLibraryPage;
+
+    @Autowired
+    DocumentTransformationEnginePage documentTransformationEnginePage;
+
+    private final String random = RandomData.getRandomAlphanumeric();
+    private final String userName = "user-" + random;
+    private String siteName = "Site-" + random;
+    private final String description = "description-" + random;
+    private String sourceFolderName = "FOR";
+    private String targetFolderName = "Transformed";
+    private String ruleName = "RuleName_" + random;
+    private String[] imageToTransform = {"Lighthouse.jpg", "png_noBackground_file.png", "tiff_file.tif"};
+
+    @BeforeClass(alwaysRun = true)
+    public void setupTest() {
+        userService.create(adminUser, adminPassword, userName, password, userName + domain, userName + "-First Name", userName + "-Last Name");
+        siteService.create(userName, password, domain, siteName, description, SiteService.Visibility.PUBLIC);
+        contentService.createFolder(userName, password, sourceFolderName, siteName);
+        contentService.createFolder(userName, password, targetFolderName, siteName);
+        setupAuthenticatedSession(userName, password);
+        editRulesPage.defineRule(ruleName, siteName, sourceFolderName, EditRulesPage.WhenRule.itemsCreatedOrEnterFolder, EditRulesPage.IfAllCriteriaAreMetRule.ALL_ITEMS, EditRulesPage.PerformActionList.TRANSFORM_AND_COPY_IMAGE);
+        performActionRulePage.transformAndCopy(PerformActionRulePage.Mimetype.BITMAP_IMAGE, siteName, targetFolderName);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void cleanup() {
+        userService.delete(adminUser, adminPassword, userName);
+        contentService.deleteTreeByPath(adminUser, adminPassword, "/User Homes/" + userName);
+        siteService.delete(adminUser, adminPassword, siteName);
+    }
+
+
+    @TestRail(id = "C239081")
+    @Test(groups = {TestGroup.SANITY, TestGroup.CONTENT})
+    public void supportedTypesTransformationToBMP() throws InterruptedException {
+
+        LOG.info("STEP 1: Go to the folder with rule and upload document.");
+        documentLibraryPage.navigate(siteName).clickOnFolderName(sourceFolderName);
+        for (int i = 0; i <= 2; i++) {
+            LOG.info("STEP: Upload " + imageToTransform[i]);
+            documentLibraryPage.uploadNewImage(String.format(testDataFolder + imageToTransform[i]));
+            assertTrue(documentLibraryPage.isContentNameDisplayed(imageToTransform[i]), String.format("File [%s] is not displayed", imageToTransform[i]));
+        }
+
+        LOG.info("STEP 2: Go to the target (e.g. Transformed space) and verify the *bmp copy of the file.");
+        documentLibraryPage.navigate(siteName).clickOnFolderName(targetFolderName);
+        for (int i = 0; i <= 2; i++) {
+            LOG.info("STEP: Check transformation of " + imageToTransform[i]);
+            assertTrue(documentLibraryPage.isContentNameDisplayed(String.format((imageToTransform[i].substring(0, imageToTransform[i].length() - 3) + "bmp"))), String.format("Transformed file [%s] is not displayed", imageToTransform[i].substring(0, imageToTransform[i].length() - 3) + "bmp"));
+        }
+
+        LOG.info("STEP 3: Open " + properties.getTransformationServerUrl() + " and verify successful info about your transformation.");
+        navigate(properties.getTransformationServerUrl().toString());
+        for (int i = 0; i <= 2; i++) {
+            LOG.info("STEP: Check in DTE server after " + imageToTransform[i]);
+            assertTrue(documentTransformationEnginePage.searchTransformation(getBrowser(), imageToTransform[i], PerformActionRulePage.Mimetype.BITMAP_IMAGE, userName), String.format("Document %s wasn't transformed!", imageToTransform[i]));
+        }
+    }
+}
