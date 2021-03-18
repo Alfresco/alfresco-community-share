f13654418519d9d3e554e646585604b7fae51a4e
Bogdan Bocancea <bogdan.bocancea@gmail.com>
2021-03-18 17:06:44 +0200

    - add wait for element to dissapear after approving task

--- alfresco-tas-share-test/src/main/java/org/alfresco/po/share/site/members/AddSiteUsersPage.java
+++ alfresco-tas-share-test/src/main/java/org/alfresco/po/share/site/members/AddSiteUsersPage.java
@@ -159,6 +159,7 @@ public class AddSiteUsersPage extends SiteCommon<AddSiteUsersPage>
     public AddSiteUsersPage assertTotalUserIs(int nrOfUsers)
     {
         String value = language.translate("addUsersPage.addedUsersTally") + nrOfUsers;
+        waitUntilElementIsVisible(addedUsersTally);
         waitUntilElementContainsText(findElement(addedUsersTally), value);
         assertEquals(getElementText(addedUsersTally), value, "Total added user is correct");
         return this;
