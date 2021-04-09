3c17515bc1cad42c48fbdc2771edad729a776c5b
Lucian Suciu <lucian.suciu@ness.com>
2021-04-09 23:14:39 +0300

    add USE_SSL env var for ssl support on share

--- packaging/docker/substituter.sh
+++ packaging/docker/substituter.sh
@@ -9,6 +9,10 @@ if [[ $REPO_PORT == "" ]]; then
    REPO_PORT=8080
 fi
 
+if [[ $USE_SSL == "true" ]]; then
+sed -ie 's_port="8080"_port="8080" scheme="https"_' /usr/local/tomcat/conf/server.xml 
+fi
+
 echo "Replace 'REPO_HOST' with '$REPO_HOST' and 'REPO_PORT' with '$REPO_PORT'"
 
 sed -i -e 's/REPO_HOST:REPO_PORT/'"$REPO_HOST:$REPO_PORT"'/g' /usr/local/tomcat/shared/classes/alfresco/web-extension/share-config-custom.xml
