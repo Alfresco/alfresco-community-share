eb969ee99c99855857ea0d201adc49b200b498e2
Raja Singh <raja.singh@contractor.alfresco.com>
2021-08-02 19:41:42 +0530

    Refactor Edit Test shouldRuleDetailsEdited(C7254) and shouldRuleGetDisable(C7258)

--- alfresco-tas-share-test/src/test/java/org/alfresco/share/alfrescoContent/applyingRulesToFolders/workingWithASetOfRules/EditRulesTests.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/share/alfrescoContent/applyingRulesToFolders/workingWithASetOfRules/EditRulesTests.java
@@ -1,135 +1,140 @@
 package org.alfresco.share.alfrescoContent.applyingRulesToFolders.workingWithASetOfRules;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.alfresco.dataprep.CMISUtil;
-import org.alfresco.dataprep.SiteService;
+import org.alfresco.po.share.DeleteDialog;
 import org.alfresco.po.share.alfrescoContent.SelectDestinationDialog;
 import org.alfresco.po.share.alfrescoContent.applyingRulesToFolders.EditRulesPage;
 import org.alfresco.po.share.alfrescoContent.applyingRulesToFolders.ManageRulesPage;
 import org.alfresco.po.share.alfrescoContent.applyingRulesToFolders.RuleDetailsPage;
 import org.alfresco.po.share.site.DocumentLibraryPage;
 import org.alfresco.po.share.site.ItemActions;
-import org.alfresco.share.ContextAwareWebTest;
+import org.alfresco.po.share.site.SiteDashboardPage;
+
+import org.alfresco.share.BaseTest;
 import org.alfresco.testrail.TestRail;
+
 import org.alfresco.utility.data.RandomData;
-import org.alfresco.utility.model.TestGroup;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.alfresco.utility.model.*;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Laura.Capsa
  */
-public class EditRulesTests extends ContextAwareWebTest
+public class EditRulesTests extends BaseTest
 {
-    private final String userName = "user-" + RandomData.getRandomAlphanumeric();
-    private final String siteName = "Site-" + RandomData.getRandomAlphanumeric();
-    private final String description = "description-" + RandomData.getRandomAlphanumeric();
+    private final String random = RandomData.getRandomAlphanumeric();
+    private final String description = "description-" + random;
     private final String path = "Documents";
-    //@Autowired
+    private final String ruleName = "rule-C7254-" + random;
+
     private DocumentLibraryPage documentLibraryPage;
-    //@Autowired
+    private SiteDashboardPage siteDashboardPage;
     private ManageRulesPage manageRulesPage;
-    //@Autowired
     private EditRulesPage editRulesPage;
-    //@Autowired
     private RuleDetailsPage ruleDetailsPage;
-    //@Autowired
     private SelectDestinationDialog selectDestinationDialog;
-    private String folderName, ruleName;
+    private DeleteDialog deleteDialog;
+    private FolderModel folderToCheck;
 
-    @BeforeClass (alwaysRun = true)
-    public void setupTest()
+    private final ThreadLocal<UserModel> user = new ThreadLocal<>();
+    private final ThreadLocal<SiteModel> site = new ThreadLocal<>();
+    private final ThreadLocal<RuleDetailsPage> ruleName1 = new ThreadLocal<>();
+
+    @BeforeMethod(alwaysRun = true) public void setupTest()
     {
-        ruleName = "rule-" + RandomData.getRandomAlphanumeric();
-        folderName = "folder-" + RandomData.getRandomAlphanumeric();
-        userService.create(adminUser, adminPassword, userName, password, userName + domain, "First Name", "Last Name");
-        siteService.create(userName, password, domain, siteName, description, SiteService.Visibility.PUBLIC);
-        contentService.createFolder(userName, password, folderName, siteName);
-        setupAuthenticatedSession(userName, password);
+        user.set(getDataUser().usingAdmin().createRandomTestUser());
+        site.set(getDataSite().usingUser(user.get()).createPublicRandomSite());
+        getCmisApi().authenticateUser(user.get());
 
-        documentLibraryPage.navigate(siteName);
-        LOG.info("Navigate to Manage Rule page for folder");
-        documentLibraryPage.clickDocumentLibraryItemAction(folderName, ItemActions.MANAGE_RULES);
-        LOG.info("Navigate to Create rule page");
+        documentLibraryPage = new DocumentLibraryPage(webDriver);
+        siteDashboardPage = new SiteDashboardPage(webDriver);
+        manageRulesPage = new ManageRulesPage(webDriver);
+        editRulesPage = new EditRulesPage(webDriver);
+        selectDestinationDialog = new SelectDestinationDialog(webDriver);
+        ruleDetailsPage = new RuleDetailsPage(webDriver);
+        deleteDialog = new DeleteDialog(webDriver);
+        authenticateUsingCookies(user.get());
+
+        folderToCheck = FolderModel.getRandomFolderModel();
+        getCmisApi().usingSite(site.get()).createFolder(folderToCheck).assertThat().existsInRepo();
+
+        siteDashboardPage.navigate(site.get()).clickDocumentLibrary();
+
+        documentLibraryPage
+            .clickDocumentLibraryItemAction(folderToCheck.getName(), ItemActions.MANAGE_RULES);
         manageRulesPage.clickCreateRules();
-        editRulesPage.setCurrentSiteName(siteName);
-        assertEquals(editRulesPage.getRelativePath(), "share/page/site/" + siteName + "/rule-edit", "Redirected to=");
-        LOG.info("Fill in Create Rule details and submit form");
+
+        editRulesPage.setCurrentSiteName(site.get().getId());
+        editRulesPage.assertCurrentSiteRelativePathEquals(site.get().getId());
+
         List<Integer> indexOfOptionFromDropdown = Arrays.asList(0, 0, 2);
         editRulesPage.typeRuleDetails(ruleName, description, indexOfOptionFromDropdown);
-        selectDestinationDialog.clickSite(siteName);
+        selectDestinationDialog.clickSite(site.get().getId());
         selectDestinationDialog.clickPathFolder(path);
         selectDestinationDialog.clickOkButton();
         editRulesPage.clickCreateButton();
-        editRulesPage.cleanupSelectedValues();
-
     }
 
+    @TestRail(id = "C7254")
 
-    @AfterClass (alwaysRun = true)
-    public void cleanup()
+    @Test(groups = { TestGroup.SANITY, TestGroup.CONTENT,
+        "tobefixed" }) public void shouldRuleDetailsEdited()
     {
-        userService.delete(adminUser, adminPassword, userName);
-        contentService.deleteTreeByPath(adminUser, adminPassword, "/User Homes/" + userName);
-        siteService.delete(adminUser, adminPassword, siteName);
-    }
-
-    @TestRail (id = "C7254")
-    @Test (groups = { TestGroup.SANITY, TestGroup.CONTENT, "tobefixed" })
-    public void editRuleDetails()
-    {
-        documentLibraryPage.navigate(siteName);
-        documentLibraryPage.clickDocumentLibraryItemAction(folderName, ItemActions.MANAGE_RULES);
-
         String updatedRuleName = "updateRule-C7254-" + RandomData.getRandomAlphanumeric();
         String updatedDescription = "Updated Rule description";
-        LOG.info("STEP1: Click 'Edit' button for rule");
+
         ruleDetailsPage.clickButton("edit");
-        assertEquals(editRulesPage.getRulePageHeader(), String.format(language.translate("rules.editPageHeader"), ruleName), "Page header=");
-        LOG.info("STEP2: Fill in Create Rule details with new details and submit form");
+        editRulesPage.assertRulesPageHeaderEquals(ruleName);
+
         List<Integer> indexOfOptionFromDropdown = Arrays.asList(1, 0, 2);
-        editRulesPage.typeRuleDetails(updatedRuleName, updatedDescription, indexOfOptionFromDropdown);
-//        editRulesPage.clickCopySelectButton();
-        selectDestinationDialog.clickSite(siteName);
+        editRulesPage
+            .typeRuleDetails(updatedRuleName, updatedDescription, indexOfOptionFromDropdown);
+        editRulesPage.clickCopySelectButton();
+        selectDestinationDialog.clickSite(site.get().getId());
         selectDestinationDialog.clickPathFolder(path);
         selectDestinationDialog.clickOkButton();
         editRulesPage.clickSaveButton();
-//        assertEquals(manageRulesPage.getPageTitle(), "Alfresco » Folder Rules", "Displayed page=");
-        ArrayList<String> expectedDescriptionDetails = new ArrayList<>(Arrays.asList("Active", "Run in background", "Rule applied to subfolders"));
-        assertEquals(ruleDetailsPage.getRuleTitle(), updatedRuleName, "Rule title=");
-        assertEquals(ruleDetailsPage.getRuleDescription(), updatedDescription, "Rule description=");
-        assertEquals(ruleDetailsPage.getDetailsList().toString(), expectedDescriptionDetails.toString(), "Description details=");
-        assertEquals(ruleDetailsPage.getWhenCondition(), editRulesPage.getSelectedOptionFromDropdown().get(0), "'When' criteria section=");
-        assertEquals(ruleDetailsPage.getIfAllCriteriaCondition(), editRulesPage.getSelectedOptionFromDropdown().get(1), "'If all criteria are met' section=");
-        assertEquals(ruleDetailsPage.getPerformAction(), "Copy items to .../documentLibrary", "'Perform Action' section=");
+
+        ArrayList<String> expectedDescriptionDetails = new ArrayList<>(
+            Arrays.asList("Active", "Run in background", "Rule applied to subfolders"));
+        ruleDetailsPage.assertRulesPageTitleEquals(updatedRuleName);
+        ruleDetailsPage.assertRuleDescriptionEquals(updatedDescription);
+        ruleDetailsPage.assertRuleDetailsListEquals(expectedDescriptionDetails.toString());
+        ruleDetailsPage
+            .assertWhenConditionEquals(editRulesPage.getSelectedOptionFromDropdown().get(3));
+        ruleDetailsPage.assertIfAllCriteriaConditionEquals(
+            editRulesPage.getSelectedOptionFromDropdown().get(1));
+        ruleDetailsPage.assertPerformedActionEquals("Copy items to .../documentLibrary");
+    }
+
+    @TestRail(id = "C7258") @Test(groups = { TestGroup.SANITY, TestGroup.CONTENT,
+        "tobefixed" }) public void shouldRuleGetDisable()
+    {
+        ruleDetailsPage.clickButton("edit");
+        editRulesPage.assertRulesPageHeaderEquals(ruleName);
+
+        editRulesPage.clickDisableRuleCheckbox();
+        editRulesPage.clickSaveButton();
+        manageRulesPage.assertManageRulesPageTitleEquals("Alfresco » Folder Rules");
+
+        FileModel fileModel = FileModel.getRandomFileModel(FileType.TEXT_PLAIN, "Test content");
+        getCmisApi().usingSite(site.get()).usingResource(folderToCheck).createFile(fileModel);
+
+        documentLibraryPage.navigate(site.get().getId());
+        documentLibraryPage.assertDocumentLibraryPageTitleEquals("Alfresco » Document Library");
+        documentLibraryPage.assertContentNameNotDisplayedFalse(fileModel.getName());
         editRulesPage.cleanupSelectedValues();
     }
 
-    @TestRail (id = "C7258")
-    @Test (groups = { TestGroup.SANITY, TestGroup.CONTENT, "tobefixed" })
-    public void disableRule()
+    @AfterMethod(alwaysRun = true) public void afterMethod()
     {
-        String fileName = "fileC7258-" + RandomData.getRandomAlphanumeric();
-        LOG.info("STEP1: Click 'Edit' button for rule");
-        ruleDetailsPage.clickButton("edit");
-        assertEquals(editRulesPage.getRulePageHeader(), String.format(language.translate("rules.editPageHeader"), ruleName), "Page header=");
-        LOG.info("STEP2: Check \"Disable rule\" checkbox.\n" + "Click \"Save\" button");
-        editRulesPage.clickDisableRuleCheckbox();
-        editRulesPage.clickSaveButton();
-//        assertEquals(manageRulesPage.getPageTitle(), "Alfresco » Folder Rules", "Displayed page=");
-        LOG.info("STEP3: Create a file in folder and verify if rule is applied");
-        contentService.createDocumentInFolder(userName, password, siteName, folderName, CMISUtil.DocumentType.HTML, fileName, "docContent");
-        documentLibraryPage.navigate(siteName);
-//        assertEquals(documentLibraryPage.getPageTitle(), "Alfresco » Document Library", "Displayed page:");
-        assertFalse(documentLibraryPage.isContentNameDisplayed(fileName), fileName + " displayed in Document Library");
-        editRulesPage.cleanupSelectedValues();
+        deleteUsersIfNotNull(user.get());
+        deleteSitesIfNotNull(site.get());
     }
 }
