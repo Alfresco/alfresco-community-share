e7a7abca001198275d1fac450e5ab4e4614df737
alahariya <Anjali.Lahariya@contractor.alfresco.com>
2021-08-11 14:56:49 +0530

    Refactored TestCase shouldDeleteExistingRule

--- alfresco-tas-share-test/src/test/java/org/alfresco/share/alfrescoContent/applyingRulesToFolders/workingWithASetOfRules/DeleteRuleTest.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/share/alfrescoContent/applyingRulesToFolders/workingWithASetOfRules/DeleteRuleTest.java
@@ -1,102 +1,80 @@
 package org.alfresco.share.alfrescoContent.applyingRulesToFolders.workingWithASetOfRules;
 
-import static org.testng.Assert.assertEquals;
+import static org.alfresco.po.share.site.ItemActions.MANAGE_RULES;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.alfresco.dataprep.SiteService;
 import org.alfresco.po.share.DeleteDialog;
-import org.alfresco.po.share.alfrescoContent.SelectDestinationDialog;
-import org.alfresco.po.share.alfrescoContent.applyingRulesToFolders.EditRulesPage;
 import org.alfresco.po.share.alfrescoContent.applyingRulesToFolders.ManageRulesPage;
 import org.alfresco.po.share.alfrescoContent.applyingRulesToFolders.RuleDetailsPage;
 import org.alfresco.po.share.site.DocumentLibraryPage;
-import org.alfresco.po.share.site.ItemActions;
-import org.alfresco.share.ContextAwareWebTest;
+import org.alfresco.po.share.site.SiteDashboardPage;
+import org.alfresco.share.alfrescoContent.applyingRulesToFolders.AbstractFolderRuleTest;
 import org.alfresco.testrail.TestRail;
 import org.alfresco.utility.data.RandomData;
+import org.alfresco.utility.model.FolderModel;
+import org.alfresco.utility.model.SiteModel;
 import org.alfresco.utility.model.TestGroup;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.alfresco.utility.model.UserModel;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-/**
- * @author Laura.Capsa
- */
-public class DeleteRuleTest extends ContextAwareWebTest
+public class DeleteRuleTest extends AbstractFolderRuleTest
 {
     private final String random = RandomData.getRandomAlphanumeric();
-    private final String userName = "user-" + random;
-    private final String siteName = "Site-" + random;
     private final String description = "description-" + random;
-    private final String path = "Documents";
     private final String ruleName = "rule-C7254-" + random;
-    private final String folderName = "Folder-C7254-" + random;
-    //@Autowired
+
+    private FolderModel folderToCheck;
     private DocumentLibraryPage documentLibraryPage;
-   // @Autowired
+    private SiteDashboardPage siteDashboardPage;
     private ManageRulesPage manageRulesPage;
-    //@Autowired
-    private EditRulesPage editRulesPage;
-    //@Autowired
     private RuleDetailsPage ruleDetailsPage;
-    //@Autowired
-    private SelectDestinationDialog selectDestinationDialog;
-    //@Autowired
     private DeleteDialog deleteDialog;
 
-    @BeforeClass (alwaysRun = true)
+    private final ThreadLocal<UserModel> user = new ThreadLocal<>();
+    private final ThreadLocal<SiteModel> site = new ThreadLocal<>();
+
+    @BeforeMethod(alwaysRun = true)
     public void setupTest()
     {
-        userService.create(adminUser, adminPassword, userName, password, userName + domain, "First Name", "Last Name");
-        siteService.create(userName, password, domain, siteName, description, SiteService.Visibility.PUBLIC);
+        user.set(getDataUser().usingAdmin().createRandomTestUser());
+        site.set(getDataSite().usingUser(user.get()).createPublicRandomSite());
+        getCmisApi().authenticateUser(user.get());
 
-        contentService.createFolder(userName, password, folderName, siteName);
+        folderToCheck = FolderModel.getRandomFolderModel();
+        getCmisApi().usingSite(site.get()).createFolder(folderToCheck).assertThat().existsInRepo();
 
-        setupAuthenticatedSession(userName, password);
-        documentLibraryPage.navigate(siteName);
-//        assertEquals(documentLibraryPage.getPageTitle(), "Alfresco » Document Library", "Displayed page:");
+        documentLibraryPage = new DocumentLibraryPage(webDriver);
+        siteDashboardPage = new SiteDashboardPage(webDriver);
+        manageRulesPage = new ManageRulesPage(webDriver);
+        ruleDetailsPage = new RuleDetailsPage(webDriver);
+        deleteDialog = new DeleteDialog(webDriver);
 
-        LOG.info("Navigate to Manage Rule page for folder");
-        documentLibraryPage.selectItemAction(folderName, ItemActions.MANAGE_RULES);
-//        assertEquals(manageRulesPage.getPageTitle(), "Alfresco » Folder Rules", "Displayed page=");
-        assertEquals(manageRulesPage.getRuleTitle(), folderName + ": Rules", "Rule title=");
-
-        LOG.info("Navigate to Create rule page");
-//        manageRulesPage.clickCreateRules();
-        editRulesPage.setCurrentSiteName(siteName);
-        assertEquals(editRulesPage.getRelativePath(), "share/page/site/" + siteName + "/rule-edit", "Redirected to=");
-
-        LOG.info("Fill in Create Rule details and submit form");
-        List<Integer> indexOfOptionFromDropdown = Arrays.asList(0, 0, 2);
-        editRulesPage.typeRuleDetails(ruleName, description, indexOfOptionFromDropdown);
-        selectDestinationDialog.selectSite(siteName);
-        selectDestinationDialog.selectFolderPath(path);
-        selectDestinationDialog.confirmFolderLocation();
-        editRulesPage.clickCreateButton();
-//        assertEquals(manageRulesPage.getPageTitle(), "Alfresco » Folder Rules", "Displayed page=");
-        editRulesPage.cleanupSelectedValues();
+        authenticateUsingCookies(user.get());
     }
 
-    @AfterClass (alwaysRun = true)
-    public void cleanup()
-    {
-        userService.delete(adminUser, adminPassword, userName);
-        contentService.deleteTreeByPath(adminUser, adminPassword, "/User Homes/" + userName);
-        siteService.delete(adminUser, adminPassword, siteName);
-    }
+    @TestRail(id = "C7267")
+    @Test(invocationCount = 3, groups = { TestGroup.SANITY, TestGroup.CONTENT })
 
-    @TestRail (id = "C7267")
-    @Test (groups = { TestGroup.SANITY, TestGroup.CONTENT, "tobefixed" })
-    public void deleteRule()
+    public void shouldDeleteExistingRule()
     {
-        LOG.info("STEP1: Click 'Delete' button for rule");
-        ruleDetailsPage.clickButton("delete");
-        assertEquals(deleteDialog.getMessage(), language.translate("documentLibrary.rules.delete.dialogMessage"), "Delete dialog=");
+        createFolderRule(user.get(), folderToCheck, "copy", ruleName, description, false, true,
+            false, "inbound");
 
-        LOG.info("STEP2: Click 'Delete' button from Delete dialog");
-        deleteDialog.confirmDeletion();
-        assertEquals(manageRulesPage.getNoRulesText(), language.translate("documentLibrary.rules.noRules"), "Displayed rules=");
+        siteDashboardPage.
+            navigate(site.get()).
+            navigateToDocumentLibraryPage();
+
+        documentLibraryPage.
+            selectItemAction(folderToCheck.getName(), MANAGE_RULES);
+
+        ruleDetailsPage.
+            clickButton("delete");
+
+        deleteDialog.
+            confirmDeletion();
+
+        manageRulesPage.
+            assertNoRulesTextIsEqual("No Rules are defined for this folder");
     }
 }
+
