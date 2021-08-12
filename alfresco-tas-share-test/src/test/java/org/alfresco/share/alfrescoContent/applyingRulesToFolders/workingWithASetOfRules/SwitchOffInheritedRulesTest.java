f326f3b1045207dd4f8f6efad3b6f70dd944b5d0
Raja Singh <raja.singh@contractor.alfresco.com>
2021-08-12 17:56:21 +0530

    Refactored Testcase SwitchOffInheritedRulesTest

--- alfresco-tas-share-test/src/test/java/org/alfresco/share/alfrescoContent/applyingRulesToFolders/workingWithASetOfRules/SwitchOffInheritedRulesTest.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/share/alfrescoContent/applyingRulesToFolders/workingWithASetOfRules/SwitchOffInheritedRulesTest.java
@@ -1,115 +1,130 @@
 package org.alfresco.share.alfrescoContent.applyingRulesToFolders.workingWithASetOfRules;
 
-import org.alfresco.dataprep.SiteService;
+import static org.alfresco.po.share.site.ItemActions.MANAGE_RULES;
+
+import org.alfresco.po.share.DeleteDialog;
 import org.alfresco.po.share.alfrescoContent.SelectDestinationDialog;
 import org.alfresco.po.share.alfrescoContent.applyingRulesToFolders.EditRulesPage;
 import org.alfresco.po.share.alfrescoContent.applyingRulesToFolders.ManageRulesPage;
 import org.alfresco.po.share.alfrescoContent.applyingRulesToFolders.RuleDetailsPage;
 import org.alfresco.po.share.site.DocumentLibraryPage;
-import org.alfresco.po.share.site.ItemActions;
-import org.alfresco.share.ContextAwareWebTest;
+import org.alfresco.po.share.site.SiteDashboardPage;
+
+import org.alfresco.share.alfrescoContent.applyingRulesToFolders.AbstractFolderRuleTest;
+
 import org.alfresco.testrail.TestRail;
+
 import org.alfresco.utility.data.RandomData;
-import org.alfresco.utility.model.TestGroup;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.alfresco.utility.model.*;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.Arrays;
-import java.util.List;
-
-import static org.alfresco.dataprep.CMISUtil.DocumentType.TEXT_PLAIN;
-import static org.testng.Assert.assertEquals;
 
 /**
  * @author Laura.Capsa
  */
-public class SwitchOffInheritedRulesTest extends ContextAwareWebTest
+public class SwitchOffInheritedRulesTest extends AbstractFolderRuleTest
 {
+    private static final String DOCUMENT_LIBRARY_PAGE_TITLE = "documentLibrary.browserTitle";
+    private static final String RULE_PAGE_TITLE = "documentLibrary.rules.browserTitle";
+    private static final String INHERIT_BUTTON_TEXT ="documentLibrary.rules.inheritButton";
+    private static final String INHERITED_RULE_INFO_MSG = "documentLibrary.rules.inheritRuleInfoMsg";
+    private static final String DONT_INHERIT_BTN_TEXT = "documentLibrary.rules.dontInheritRuleButton";
+
     private final String random = RandomData.getRandomAlphanumeric();
-    private final String userName = "user-" + random;
-    private final String firstName = "First Name";
-    private final String lastName = "Last Name";
-    private final String siteName = "Site-" + random;
+    private final String parentFolder = "ParentFolder-"+random;
+    private final String childFolder = "ChildFolder-"+random;
     private final String description = "description-" + random;
-    private final String ruleName = "rule-C7325-" + random;
-    private final String folder1 = "Folder1-C7325-" + random;
-    private final String folder2 = "Folder2-C7325-" + random;
-    private final String fileName = "File-C7325-" + random;
-    private final String path = "Sites/" + siteName + "/documentLibrary/" + folder1;
-    //@Autowired
-    SelectDestinationDialog selectDestinationDialog;
-    //@Autowired
+    private final String ruleName = "rule-C7254-" + random;
+
     private DocumentLibraryPage documentLibraryPage;
-    //@Autowired
+    private SiteDashboardPage siteDashboardPage;
     private ManageRulesPage manageRulesPage;
-    //@Autowired
     private EditRulesPage editRulesPage;
-    //@Autowired
     private RuleDetailsPage ruleDetailsPage;
+    private SelectDestinationDialog selectDestinationDialog;
+    private DeleteDialog deleteDialog;
+    private FolderModel folderToCheck;
+    private FileModel file;
 
+    private final ThreadLocal<UserModel> user = new ThreadLocal<>();
+    private final ThreadLocal<SiteModel> site = new ThreadLocal<>();
 
-    @BeforeClass ()
+    @BeforeMethod(alwaysRun = true)
     public void setupTest()
     {
-        userService.create(adminUser, adminPassword, userName, password, userName + domain, firstName, lastName);
-        siteService.create(userName, password, domain, siteName, description, SiteService.Visibility.PUBLIC);
-        contentService.createFolder(userName, password, folder1, siteName);
-        contentService.createFolderInRepository(userName, password, folder2, path);
-        setupAuthenticatedSession(userName, password);
-        documentLibraryPage.navigate(siteName);
-//        assertEquals(documentLibraryPage.getPageTitle(), "Alfresco » Document Library", "Displayed page:");
-        LOG.info("Navigate to Manage Rule page for " + folder1);
-        documentLibraryPage.selectItemAction(folder1, ItemActions.MANAGE_RULES);
-//        assertEquals(manageRulesPage.getPageTitle(), "Alfresco » Folder Rules", "Displayed page=");
-        assertEquals(manageRulesPage.getRuleTitle(), folder1 + ": Rules", "Rule title=");
-        LOG.info("Navigate to Create rule page");
-//        manageRulesPage.clickCreateRules();
-        editRulesPage.setCurrentSiteName(siteName);
-        assertEquals(editRulesPage.getRelativePath(), "share/page/site/" + siteName + "/rule-edit", "Redirected to=");
-        LOG.info("Fill in Create Rule details and submit form");
-        List<Integer> indexOfOptionFromDropdown = Arrays.asList(0, 0, 2);
-        editRulesPage.typeRuleDetails(ruleName, description, indexOfOptionFromDropdown);
-        selectDestinationDialog.selectSite(siteName);
-        selectDestinationDialog.selectFolderPath(path);
-        selectDestinationDialog.confirmFolderLocation();
-        editRulesPage.clickCreateButton();
-//        assertEquals(ruleDetailsPage.getPageTitle(), "Alfresco » Folder Rules", "Displayed page=");
-        editRulesPage.cleanupSelectedValues();
-        LOG.info("Navigate inside " + folder1);
-        documentLibraryPage.navigate(siteName);
-//        assertEquals(documentLibraryPage.getPageTitle(), "Alfresco » Document Library", "Displayed page:");
-        documentLibraryPage.clickOnFolderName(folder1);
-        assertEquals(documentLibraryPage.getBreadcrumbList(), Arrays.asList("Documents", folder1).toString(), "Document Library breadcrumb=");
+        user.set(getDataUser().usingAdmin().createRandomTestUser());
+        site.set(getDataSite().usingUser(user.get()).createPublicRandomSite());
+        getCmisApi().authenticateUser(user.get());
+
+        documentLibraryPage = new DocumentLibraryPage(webDriver);
+        siteDashboardPage = new SiteDashboardPage(webDriver);
+        manageRulesPage = new ManageRulesPage(webDriver);
+        editRulesPage = new EditRulesPage(webDriver);
+        selectDestinationDialog = new SelectDestinationDialog(webDriver);
+        ruleDetailsPage = new RuleDetailsPage(webDriver);
+        deleteDialog = new DeleteDialog(webDriver);
+        authenticateUsingCookies(user.get());
+
+        folderToCheck = FolderModel.getRandomFolderModel();
+        folderToCheck.setName(parentFolder);
+
+        getCmisApi()
+            .usingSite(site.get())
+            .createFolder(folderToCheck)
+            .assertThat().existsInRepo();
+
+        createFolderRule(user.get(),
+            folderToCheck, "copy",
+            ruleName, description,
+            false, true, false, "inbound");
+
+        folderToCheck.setName(childFolder);
+        getCmisApi()
+            .usingSite(site.get())
+            .usingResource(folderToCheck)
+            .createFolder(folderToCheck);
     }
 
-    @AfterClass (alwaysRun = true)
-    public void cleanup()
+    @TestRail(id = "C7325")
+    @Test(groups = { TestGroup.SANITY, TestGroup.CONTENT })
+    public void shouldSwitchOffTheInheritRules()
     {
-        userService.delete(adminUser, adminPassword, userName);
-        contentService.deleteTreeByPath(adminUser, adminPassword, "/User Homes/" + userName);
-        siteService.delete(adminUser, adminPassword, siteName);
+        siteDashboardPage
+            .navigate(site.get())
+            .navigateToDocumentLibraryPage();
+
+        documentLibraryPage
+            .clickOnFolderName(parentFolder)
+            .selectItemAction(childFolder, MANAGE_RULES);
+
+        manageRulesPage
+            .assertInheritButtonTextEquals(language.translate(INHERIT_BUTTON_TEXT))
+            .assertInheritRuleInfoMessageEquals(language.translate(INHERITED_RULE_INFO_MSG))
+            .clickInheritButton()
+            .assertInheritButtonTextEquals(language.translate(DONT_INHERIT_BTN_TEXT));
+
+        documentLibraryPage
+            .navigate(site.get())
+            .assertDocumentLibraryPageTitleEquals(language.translate(DOCUMENT_LIBRARY_PAGE_TITLE))
+            .clickOnFolderName(parentFolder)
+            .clickOnFolderName(childFolder);
+
+        file = FileModel.getRandomFileModel(FileType.TEXT_PLAIN);
+        getCmisApi().usingSite(site.get()).usingResource(folderToCheck).usingResource(folderToCheck)
+            .createFile(file);
+
+        documentLibraryPage
+            .assertFileIsDisplayed(file.getName())
+            .navigate(site.get())
+            .clickOnFolderName(parentFolder)
+            .assertFileIsNotDisplayed(file.getName());
     }
 
-    @TestRail (id = "C7325")
-    @Test (groups = { TestGroup.SANITY, TestGroup.CONTENT, "tobefixed" })
-    public void switchOffInheritRules()
+    @AfterMethod(alwaysRun = true) public void afterMethod()
     {
-        LOG.info("STEP1: Navigate to 'Manage Rules' page for " + folder2);
-        documentLibraryPage.selectItemAction(folder2, ItemActions.MANAGE_RULES);
-//        assertEquals(manageRulesPage.getPageTitle(), "Alfresco » Folder Rules", "Displayed page=");
-        assertEquals(manageRulesPage.getRuleTitle(), folder2 + ": Rules", "Rule title=");
-        LOG.info("STEP2: Click on 'Inherit Rules' button");
-        manageRulesPage.clickInheritButton();
-        assertEquals(manageRulesPage.getInheritButtonText(), "Don't Inherit Rules", "Inherit button text=");
-        LOG.info("STEP3: Navigate to Document Library -> 'Folder1'");
-        documentLibraryPage.navigate(siteName);
-//        assertEquals(documentLibraryPage.getPageTitle(), "Alfresco » Document Library", "Displayed page:");
-        documentLibraryPage.clickOnFolderName(folder1);
-        documentLibraryPage.clickOnFolderName(folder2);
-        assertEquals(documentLibraryPage.getBreadcrumbList(), Arrays.asList("Documents", folder1, folder2).toString(), "Document Library breadcrumb=");
-        LOG.info("STEP4: Create a file");
-        contentService.createDocumentInRepository(userName, password, path + "/" + folder2, TEXT_PLAIN, fileName, "content of Document");
-        assertEquals(documentLibraryPage.getFilesList(), Arrays.asList(fileName), folder2 + " content=");
+        deleteUsersIfNotNull(user.get());
+        deleteSitesIfNotNull(site.get());
     }
 }
