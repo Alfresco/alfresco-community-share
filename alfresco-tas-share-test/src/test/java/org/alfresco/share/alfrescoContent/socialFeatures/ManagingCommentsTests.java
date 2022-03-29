45b3b2c69bbb1cb662336e8909ef18a51d7ffb7e
Raja Singh <raja.singh@contractor.alfresco.com>
2022-03-29 15:27:28 +0530

    Test methods 'commentsDeleteComment' & 'commentsEditComment' refactored

--- alfresco-tas-share-test/src/test/java/org/alfresco/share/alfrescoContent/socialFeatures/ManagingCommentsTests.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/share/alfrescoContent/socialFeatures/ManagingCommentsTests.java
@@ -1,96 +1,128 @@
 package org.alfresco.share.alfrescoContent.socialFeatures;
 
-import org.alfresco.dataprep.CMISUtil;
-import org.alfresco.dataprep.SiteService;
+import lombok.extern.slf4j.Slf4j;
 import org.alfresco.po.share.alfrescoContent.document.DocumentDetailsPage;
 import org.alfresco.po.share.site.DocumentLibraryPage;
-import org.alfresco.share.ContextAwareWebTest;
+import org.alfresco.share.BaseTest;
 import org.alfresco.testrail.TestRail;
 import org.alfresco.utility.data.RandomData;
-import org.alfresco.utility.model.TestGroup;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.alfresco.utility.model.*;
+import org.testng.annotations.*;
 
-public class ManagingCommentsTests extends ContextAwareWebTest
+@Slf4j
+public class ManagingCommentsTests extends BaseTest
 {
-    private final String user = String.format("C9934User%s", RandomData.getRandomAlphanumeric());
-    private final String siteName = String.format("C9934SiteName%s", RandomData.getRandomAlphanumeric());
-    private final String description = String.format("C9934SiteDescription%s", RandomData.getRandomAlphanumeric());
-    private final String docName = "File-C9934";
-    private final String comment = "Test comment for C9934-1";
-    private final String editedComment = "Test comment edited for C9934";
-    //@Autowired
-    private DocumentDetailsPage documentPreviewPage;
-    //@Autowired
-    private DocumentLibraryPage documentLibraryPage;
+    private static final String NO_COMMENTS = "documentLibrary.documentDetailsPage.commentSection.noComment";
+    private final String random = RandomData.getRandomAlphanumeric();
+    private final String description = "description-" + random;
+    private final String comment = "Test comment for C9934" + random;
+    private final String editedComment = "Test comment edited for C9934" + random;
 
-    @BeforeClass (alwaysRun = true)
+    private FileModel fileToCheck;
+    private DocumentLibraryPage documentLibraryPage;
+    private DocumentDetailsPage documentPreviewPage;
+
+    private final ThreadLocal<UserModel> user = new ThreadLocal<>();
+    private final ThreadLocal<SiteModel> site = new ThreadLocal<>();
+
+    @BeforeMethod(alwaysRun = true)
     public void setupTest()
     {
-        userService.create(adminUser, adminPassword, user, password, user + domain, user, user);
-        siteService.create(user, password, domain, siteName, description, SiteService.Visibility.PUBLIC);
-        contentService.createDocument(user, password, siteName, CMISUtil.DocumentType.TEXT_PLAIN, docName, "content");
-        setupAuthenticatedSession(user, password);
+        user.set(getDataUser().usingAdmin().createRandomTestUser());
+        site.set(getDataSite().usingUser(user.get()).createPublicRandomSite());
+        getCmisApi().authenticateUser(user.get());
+
+        fileToCheck = FileModel.getRandomFileModel(FileType.TEXT_PLAIN, description);
+
+        getCmisApi()
+            .usingSite(site.get())
+            .createFile(fileToCheck)
+            .assertThat()
+            .existsInRepo();
+
+        documentLibraryPage = new DocumentLibraryPage(webDriver);
+        documentPreviewPage = new DocumentDetailsPage(webDriver);
+
+        authenticateUsingCookies(user.get());
     }
 
-    @AfterClass (alwaysRun = true)
-    public void cleanup()
+    @AfterMethod(alwaysRun = true)
+    public void afterMethod()
     {
-        userService.delete(adminUser, adminPassword, user);
-        contentService.deleteTreeByPath(adminUser, adminPassword, "/User Homes/" + user);
-        siteService.delete(adminUser, adminPassword, siteName);
+        deleteUsersIfNotNull(user.get());
+        deleteSitesIfNotNull(site.get());
     }
 
     @TestRail (id = "C9934")
     @Test (groups = { TestGroup.SANITY, TestGroup.CONTENT })
     public void commentsDeleteComment()
     {
-        LOG.info("Precondition: Add a comment to the file created in the site.");
-        documentLibraryPage.navigate(siteName);
-        documentLibraryPage.clickOnFile(docName);
-        documentPreviewPage.clickOnCommentDocument();
-        documentPreviewPage.addComment(comment);
-        Assert.assertEquals(documentPreviewPage.getCommentContent(comment), comment);
+        log.info("Precondition: click on the file created in the site where comment to be added.");
+        documentLibraryPage
+            .navigate(site.get().getTitle())
+            .clickOnFile(fileToCheck.getName())
+            .clickOnCommentDocument();
 
-        LOG.info("Step 1: Hover mouse over the comment and check that the Delete button is displayed");
-        Assert.assertTrue(documentPreviewPage.isDeleteButtonDisplayedForComment(comment), "Delete button is not displayed for comment");
+        log.info("Step 1: Add comment to the file");
+        documentPreviewPage
+            .addComment(comment)
+            .assertVerifyCommentContent(comment);
 
-        LOG.info("Step 2: Click Delete and check that the Delete Prompt is displayed");
-        documentPreviewPage.clickDeleteComment(comment);
-        Assert.assertTrue(documentPreviewPage.isDeleteCommentPromptDisplayed(), "Delete Comment prompt is not displayed");
+        log.info("Step 2: Hover mouse over the comment and check that the Delete button is displayed");
+        documentPreviewPage
+            .assertIsDeleteButtonDisplayedForComment(comment);
 
-        LOG.info("Step 3: Click Delete button on the Delete Comment prompt");
-        documentPreviewPage.clickDeleteOnDeleteComment();
-        //getBrowser().waitUntilElementVisible(documentPreviewPage.noComments);
-        Assert.assertEquals(documentPreviewPage.getNoCommentsText(), "No comments", "No comments notification is not displayed");
+        log.info("Step 3: Click Delete and check that the Delete Prompt is displayed");
+        documentPreviewPage
+            .clickDeleteComment(comment);
+
+        documentPreviewPage
+            .assertIsDeleteCommentPromptDisplayed();
+
+        log.info("Step 4: Click Delete button on the Delete Comment prompt");
+        documentPreviewPage
+            .clickDeleteOnDeleteComment();
+
+        log.info("Step 5: Verify the 'No Comment' notification text after deleting the comment");
+        documentPreviewPage
+            .assertNoCommentNotificationText(language.translate(NO_COMMENTS));
     }
 
     @TestRail (id = "C9935")
     @Test (groups = { TestGroup.SANITY, TestGroup.CONTENT })
-
     public void commentsEditComment()
     {
-        LOG.info("Precondition: Add a comment to the file created in the site.");
-        documentLibraryPage.navigate(siteName);
-        documentLibraryPage.clickOnFile(docName);
-        documentPreviewPage.clickOnCommentDocument();
-        documentPreviewPage.addComment(comment);
-        Assert.assertEquals(documentPreviewPage.getCommentContent(comment), comment);
+        log.info("Precondition: click on the file created in the site where comment to be added.");
+        documentLibraryPage
+            .navigate(site.get().getTitle())
+            .clickOnFile(fileToCheck.getName())
+            .clickOnCommentDocument();
 
-        LOG.info("Step 1: Hover mouse over the comment and check that the Edit button is displayed");
-        Assert.assertTrue(documentPreviewPage.isEditButtonDisplayedForComment(comment), "Edit button is not displayed for comment");
+        log.info("Step 1: Add comment to the file and then verify the comment ");
+        documentPreviewPage
+            .addComment(comment)
+            .assertVerifyCommentContent(comment);
 
-        LOG.info("Step 2: Click the Edit button");
-        documentPreviewPage.clickOnEditComment(comment);
-        Assert.assertTrue(documentPreviewPage.isEditCommentDisplayed(), "Edit comment is not displayed");
+        log.info("Step 2: Hover mouse over the comment and check that the Edit button is displayed");
+        documentPreviewPage
+            .assertIsEditButtonDisplayedForComment(comment);
 
-        LOG.info("Step 3: Edit comment text and click on Save");
-        documentPreviewPage.editComment(editedComment);
-        documentPreviewPage.clickOnSaveButtonEditComment();
-        getBrowser().waitInSeconds(3);
-        Assert.assertEquals(documentPreviewPage.getCommentContent(editedComment), editedComment, "Edited comment text is not correct");
+        log.info("Step 3: Click the Edit button and verify edit comment displayed");
+        documentPreviewPage
+            .clickOnEditComment(comment);
+
+        log.info("Step 4: Verify 'Edit Comment' title of the comment box is displayed");
+        documentPreviewPage
+            .assertIsEditCommentTitleDisplayed();
+
+        log.info("Step 5: Edit comment text and click on Save");
+        documentPreviewPage
+            .editComment(editedComment);
+        documentPreviewPage
+            .clickOnSaveButtonEditComment();
+
+        log.info("Step 6: Verify the edited comment content");
+        documentPreviewPage
+            .assertVerifyCommentContent(editedComment);
     }
 }
