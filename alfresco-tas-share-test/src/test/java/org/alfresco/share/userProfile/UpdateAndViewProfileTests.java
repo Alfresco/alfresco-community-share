91cdad02ee516cd79477b104137c452ac926df93
kshah <kavit.shah@globallogic.com>
2022-06-23 14:07:18 +0530

    Revert "Fixing User and Authentication Tests Failure"
    This reverts commit 9fa4bc317adb79200d4f03f8d4f593d26d6b0555.

--- alfresco-tas-share-test/src/test/java/org/alfresco/share/userProfile/UpdateAndViewProfileTests.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/share/userProfile/UpdateAndViewProfileTests.java
@@ -1,15 +1,13 @@
 package org.alfresco.share.userProfile;
 
+import org.alfresco.common.Utils;
 import org.alfresco.po.share.user.profile.EditUserProfilePage;
 import org.alfresco.po.share.user.profile.UserProfilePage;
 import org.alfresco.share.BaseTest;
 import org.alfresco.testrail.TestRail;
 import org.alfresco.utility.model.TestGroup;
 import org.alfresco.utility.model.UserModel;
-import org.apache.commons.lang.StringUtils;
 import org.testng.annotations.*;
-
-import java.io.File;
 
 public class UpdateAndViewProfileTests extends BaseTest
 {
@@ -87,9 +85,7 @@ public class UpdateAndViewProfileTests extends BaseTest
         userProfilePage.navigate(user.get())
             .assertDefaultAvatarIsDisplayed()
             .clickEditProfile()
-            .uploadNewPhoto(StringUtils.substringBeforeLast(System.getProperty("user.dir"),File.separator)
-                + File.separator + "testdata" + File.separator
-                + "newavatar.jpg")
+            .uploadNewPhoto(Utils.testDataFolder + "newavatar.jpg")
             .clickSave()
             .assertNewAvatarIsDisplayed()
             .clickEditProfile()
