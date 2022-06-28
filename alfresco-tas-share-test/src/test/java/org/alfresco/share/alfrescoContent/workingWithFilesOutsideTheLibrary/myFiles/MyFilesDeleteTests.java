f1bab73a45c3e0c1a74fda53fe48af74883e4c81
ggnanadhas <godwin.gnanadhas@globallogic.com>
2022-06-28 16:31:03 +0530

    Test Refactored

--- alfresco-tas-share-test/src/test/java/org/alfresco/share/alfrescoContent/workingWithFilesOutsideTheLibrary/myFiles/MyFilesDeleteTests.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/share/alfrescoContent/workingWithFilesOutsideTheLibrary/myFiles/MyFilesDeleteTests.java
@@ -1,91 +1,131 @@
 package org.alfresco.share.alfrescoContent.workingWithFilesOutsideTheLibrary.myFiles;
 
+import static org.alfresco.common.Utils.testDataFolder;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
+import lombok.extern.slf4j.Slf4j;
 import org.alfresco.dataprep.CMISUtil;
 import org.alfresco.po.share.DeleteDialog;
 import org.alfresco.po.share.MyFilesPage;
+import org.alfresco.po.share.alfrescoContent.buildingContent.NewFolderDialog;
+import org.alfresco.po.share.alfrescoContent.document.DocumentDetailsPage;
+import org.alfresco.po.share.alfrescoContent.document.SocialFeatures;
+import org.alfresco.po.share.alfrescoContent.document.UploadContent;
+import org.alfresco.po.share.site.DocumentLibraryPage;
 import org.alfresco.po.share.site.ItemActions;
+import org.alfresco.po.share.site.SiteDashboardPage;
+import org.alfresco.share.BaseTest;
 import org.alfresco.share.ContextAwareWebTest;
 import org.alfresco.testrail.TestRail;
 import org.alfresco.utility.data.RandomData;
 import org.alfresco.utility.model.TestGroup;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.alfresco.utility.model.UserModel;
+import org.testng.annotations.*;
 
+@Slf4j
 /**
  * @author Razvan.Dorobantu
  */
-public class MyFilesDeleteTests extends ContextAwareWebTest
+public class MyFilesDeleteTests extends BaseTest
 {
-    private final String user = String.format("user%s", RandomData.getRandomAlphanumeric());
-    private final String testFile = String.format("testFile%s", RandomData.getRandomAlphanumeric());
-    private final String folderName = String.format("testFolder%s", RandomData.getRandomAlphanumeric());
-    private final String myFilesPath = "User Homes/" + user;
-   // @Autowired
+    private final String testFile = RandomData.getRandomAlphanumeric() + "testFile.txt";
+    private final String testFilePath = testDataFolder + testFile;
+    private final String testFolder = String.format("testFolder%s", RandomData.getRandomAlphanumeric());
+
     private MyFilesPage myFilesPage;
-    //@Autowired
+
+    private SiteDashboardPage sitePage;
+    private DocumentDetailsPage documentDetailsPage;
     private DeleteDialog deleteDialog;
 
-    @BeforeClass (alwaysRun = true)
-    public void createUser()
-    {
-        userService.create(adminUser, adminPassword, user, password, user + domain, user, user);
-        setupAuthenticatedSession(user, password);
+    private NewFolderDialog newContentDialog;
+    private DocumentLibraryPage documentLibraryPage;
+    private UploadContent uploadContent;
+
+    private SocialFeatures social;
+    private UserModel testUser;
+    private final ThreadLocal<UserModel> user = new ThreadLocal<>();
+
+    @BeforeMethod(alwaysRun = true)
+    public void setupTest() {
+        log.info("PreCondition: Creating a TestUser");
+        testUser = dataUser.usingAdmin().createRandomTestUser();
+        getCmisApi().authenticateUser(testUser);
+        authenticateUsingCookies(testUser);
+
+        uploadContent = new UploadContent(webDriver);
+        myFilesPage = new MyFilesPage(webDriver);
+        documentLibraryPage = new DocumentLibraryPage(webDriver);
+        newContentDialog = new NewFolderDialog(webDriver);
+        documentDetailsPage = new DocumentDetailsPage(webDriver);
+        deleteDialog = new DeleteDialog(webDriver);
     }
 
-    @AfterClass (alwaysRun = true)
-    public void cleanup()
-    {
-        userService.delete(adminUser, adminPassword, user);
-        contentService.deleteTreeByPath(adminUser, adminPassword, "/User Homes/" + user);
-
+    @AfterMethod(alwaysRun = true)
+    public void cleanup() {
+        deleteUsersIfNotNull(user.get());
     }
-
 
     @TestRail (id = "C7896")
-    @Test (groups = { TestGroup.SANITY, TestGroup.CONTENT, "tobefixed" })
+    @Test (groups = { TestGroup.SANITY, TestGroup.CONTENT})
     public void myFilesDeleteDocument()
     {
-        LOG.info("Precondition: Login as user, navigate to My Files page and upload a file.");
-        contentService.createDocumentInRepository(user, password, myFilesPath, CMISUtil.DocumentType.TEXT_PLAIN, testFile, "some content");
-        myFilesPage.navigate();
-//        Assert.assertEquals(myFilesPage.getPageTitle(), "Alfresco » My Files");
-        assertTrue(myFilesPage.isContentNameDisplayed(testFile), String.format("The file [%s] is not present", testFile));
+        log.info("Precondition: Navigate to My Files page and upload a file.");
+        myFilesPage
+            .navigate()
+            .assertBrowserPageTitleIs("Alfresco » My Files");
+        uploadContent
+            .uploadContent(testFilePath);
+        myFilesPage
+            .assertIsContantNameDisplayed(testFile);
 
-        LOG.info("STEP1: Hover over the file. STEP2: Click 'More...' link. Click 'Delete Document' link");
-        myFilesPage.selectItemAction(testFile, ItemActions.DELETE_DOCUMENT);
-        assertEquals(deleteDialog.getHeader(), language.translate("documentLibrary.deleteDocument"), "'Delete Document' pop-up is displayed");
-        assertEquals(deleteDialog.getMessage(), String.format(language.translate("confirmDeletion.message"), testFile));
+        log.info("STEP1: Hover over the file. STEP2: Click 'More...' link. Click 'Delete Document' link");
+        myFilesPage
+            .select_ItemAction(testFile, ItemActions.DELETE_DOCUMENT)
+            .assertDialogTitleEquals("Delete Document")
+            .assertConfirmationMessage(String.format(language.translate("confirmDeletion.message"), testFile));
 
-        LOG.info("STEP3: Click 'Delete' button");
-        deleteDialog.confirmDeletion();
+        log.info("STEP3: Click 'Delete' button");
+        deleteDialog
+            .confirmDeletion();
 
-        LOG.info("STEP4: Verify that the file was deleted");
-        assertFalse(myFilesPage.isContentNameDisplayed(testFile), "Documents item list is refreshed and is empty");
+        log.info("STEP4: Verify that the file was deleted");
+        myFilesPage
+            .assertIsContentDeleted(testFile);
     }
 
     @TestRail (id = "C7896")
-    @Test (groups = { TestGroup.SANITY, TestGroup.CONTENT, "tobefixed" })
+    @Test (groups = { TestGroup.SANITY, TestGroup.CONTENT})
     public void myFilesDeleteFolder()
     {
-        LOG.info("Precondition: Login as user, navigate to My Files page and create a folder.");
-        contentService.createFolderInRepository(user, password, folderName, myFilesPath);
-        myFilesPage.navigate();
-//        Assert.assertEquals(myFilesPage.getPageTitle(), "Alfresco » My Files");
-        assertTrue(myFilesPage.isContentNameDisplayed(folderName), folderName + " displayed in My Files documents list.");
+        log.info("Precondition: Navigate to My Files page and create a folder.");
+        myFilesPage
+            .navigate()
+            .assertBrowserPageTitleIs("Alfresco » My Files");
+        myFilesPage
+            .click_CreateButton()
+            .click_FolderLink()
+            .assertDialogTitleEquals("New Folder");
+        newContentDialog
+            .typeName(testFolder)
+            .clickSave();
+        myFilesPage
+            .assertIsContantNameDisplayed(testFolder);
 
-        LOG.info("STEP1: Hover over the folder. STEP2: Click on 'More...' link and choose 'Delete Folder' from the dropdown list.");
-        myFilesPage.selectItemAction(folderName, ItemActions.DELETE_FOLDER);
-        assertEquals(deleteDialog.getHeader(), language.translate("documentLibrary.deleteFolder"), "'Delete Folder' pop-up is displayed");
-        assertEquals(deleteDialog.getMessage(), String.format(language.translate("confirmDeletion.message"), folderName));
+        log.info("STEP1: Hover over the folder. STEP2: Click on 'More...' link and choose 'Delete Folder' from the dropdown list.");
+        myFilesPage
+            .select_ItemAction(testFolder, ItemActions.DELETE_FOLDER)
+            .assertDialogTitleEquals("Delete Folder")
+            .assertConfirmationMessage(String.format(language.translate("confirmDeletion.message"), testFolder));
 
-        LOG.info("STEP3: Click 'Delete' button");
-        deleteDialog.confirmDeletion();
-        assertFalse(myFilesPage.isContentNameDisplayed(folderName), "Documents item list is refreshed and is empty");
-        assertFalse(myFilesPage.getExplorerPanelDocuments().contains(folderName), "'DelFolder' is not visible in 'Library' section of the browsing pane.");
+        log.info("STEP3: Click 'Delete' button");
+        deleteDialog
+            .confirmDeletion();
+
+        log.info("STEP4: Verify that the folder was deleted");
+        myFilesPage
+            .assertIsContentDeleted(testFolder);
     }
 }
