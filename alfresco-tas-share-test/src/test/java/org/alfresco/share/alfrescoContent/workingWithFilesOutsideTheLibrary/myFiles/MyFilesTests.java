66207b8942fb6520678c4040182764421b188b8f
ggnanadhas <godwin.gnanadhas@globallogic.com>
2022-07-28 16:26:16 +0530

    Test Methods in Class 'MyFilesTests' Refactored

--- alfresco-tas-share-test/src/test/java/org/alfresco/share/alfrescoContent/workingWithFilesOutsideTheLibrary/myFiles/MyFilesTests.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/share/alfrescoContent/workingWithFilesOutsideTheLibrary/myFiles/MyFilesTests.java
@@ -1,98 +1,153 @@
 package org.alfresco.share.alfrescoContent.workingWithFilesOutsideTheLibrary.myFiles;
 
-import org.alfresco.dataprep.SiteService;
+import lombok.extern.slf4j.Slf4j;
 import org.alfresco.po.share.MyFilesPage;
 import org.alfresco.po.share.alfrescoContent.buildingContent.CreateContentPage;
+import org.alfresco.po.share.alfrescoContent.document.UploadContent;
 import org.alfresco.po.share.site.DocumentLibraryPage.CreateMenuOption;
 import org.alfresco.po.share.toolbar.Toolbar;
 import org.alfresco.po.share.user.UserDashboardPage;
-import org.alfresco.share.ContextAwareWebTest;
+import org.alfresco.po.share.user.admin.adminTools.usersAndGroups.CreateUserPage;
+import org.alfresco.po.share.user.admin.adminTools.usersAndGroups.UsersPage;
+import org.alfresco.share.BaseTest;
 import org.alfresco.testrail.TestRail;
 import org.alfresco.utility.data.RandomData;
 import org.alfresco.utility.model.TestGroup;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.alfresco.utility.model.UserModel;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class MyFilesTests extends ContextAwareWebTest
+import static org.alfresco.common.Utils.testDataFolder;
+
+@Slf4j
+public class MyFilesTests extends BaseTest
 {
-    private final String user = String.format("C7648TestUser%s", RandomData.getRandomAlphanumeric());
-    private final String user1 = String.format("C7648TestUser1%s", RandomData.getRandomAlphanumeric());
+    private final String user1 = String.format("C7648TestUser%s", RandomData.getRandomAlphanumeric());
+    private final String user2 = String.format("C7648TestUser1%s", RandomData.getRandomAlphanumeric());
     private final String siteName = String.format("C7658SiteName%s", RandomData.getRandomAlphanumeric());
-    private final String description = String.format("C7658SiteDescription%s", RandomData.getRandomAlphanumeric());
+    private final String testFile = RandomData.getRandomAlphanumeric() + "testFile.txt";
+    private final String testFilePath = testDataFolder + testFile;
     private final String C7648name = "C7648 name";
     private final String C7648title = "C7648 title";
     private final String C7648content = "C7648 content";
     private final String C7648description = "C7648 description";
+    private final String password = "password";
    // @Autowired
     private MyFilesPage myFilesPage;
+
     //@Autowired
     private CreateContentPage create;
     //@Autowired
     private UserDashboardPage userDashboard;
     //@Autowired
     private Toolbar toolbar;
+    private UploadContent uploadContent;
+    private CreateUserPage createUsers;
+    private UsersPage usersPage;
+    private final ThreadLocal<UserModel> user = new ThreadLocal<>();
 
-    @BeforeClass (alwaysRun = true)
+    @BeforeMethod(alwaysRun = true)
     public void setupTest()
     {
-        userService.create(adminUser, adminPassword, user, password, user + domain, user, user);
-        siteService.create(user, password, domain, siteName, description, SiteService.Visibility.PUBLIC);
-        userService.create(adminUser, adminPassword, user1, password, user1 + domain, user1, user1);
-        setupAuthenticatedSession(user, password);
+        toolbar = new Toolbar(webDriver);
+        userDashboard = new UserDashboardPage(webDriver);
+        myFilesPage = new MyFilesPage(webDriver);
+        create = new CreateContentPage(webDriver);
+        createUsers = new CreateUserPage(webDriver);
+        uploadContent = new UploadContent(webDriver);
+        usersPage = new UsersPage(webDriver);
     }
 
-    @AfterClass (alwaysRun = true)
+    @AfterMethod(alwaysRun = true)
     public void cleanup()
     {
-        userService.delete(adminUser, adminPassword, user);
-        contentService.deleteTreeByPath(adminUser, adminPassword, "/User Homes/" + user);
-
-        userService.delete(adminUser, adminPassword, user1);
-        contentService.deleteTreeByPath(adminUser, adminPassword, "/User Homes/" + user1);
-
-        siteService.delete(adminUser, adminPassword, siteName);
+        deleteUsersIfNotNull(user.get());
     }
-
-
     @TestRail (id = "C7648")
     @Test (groups = { TestGroup.SANITY, TestGroup.CONTENT })
-    public void myFilesContentAvailability()
-    {
-        LOG.info("Step 1: While logged in with user create Plain Text document in My Files");
-        myFilesPage.navigate();
+    public void myFilesContentAvailability() {
 
-        myFilesPage.clickCreateButton();
-        myFilesPage.clickCreateContentOption(CreateMenuOption.PLAIN_TEXT);
-        create.typeName(C7648name);
-        create.typeContent(C7648content);
-        create.typeTitle(C7648title);
-        create.typeDescription(C7648description);
-        create.clickCreate();
-        myFilesPage.navigate();
-        Assert.assertTrue(myFilesPage.isContentNameDisplayed(C7648name), "C7648 name is not displayed in My Files");
-        userService.logout();
+        log.info("PreCondition: Two users are created, e.g: user1 and user2 ");
+        authenticateUsingLoginPage(getAdminUser());
+        usersPage
+            .navigate()
+            .clickNewUserButton()
+            .setFirstName("TestUser1")
+            .setEmail("user1@test.com")
+            .setUsername(user1)
+            .setPassword(password)
+            .setVerifyPassword(password)
+            .clickCreateUserAndStartAnother();
 
-        LOG.info("Step 2: Login with user1 and check that the file create by user is not visible in My Files");
-        setupAuthenticatedSession(user1, password);
-        myFilesPage.navigate();
-        Assert.assertFalse(myFilesPage.isContentNameDisplayed(C7648name), "C7648 name is displayed in My Files");
+        createUsers
+            .setFirstName("TestUser2")
+            .setEmail("user2@test.com")
+            .setUsername(user2)
+            .setPassword(password)
+            .setVerifyPassword(password)
+            .clickCreate();
+
+        log.info("PreCondition: Verify two users are created, e.g: user1 and user2 ");
+        usersPage
+            .searchUserWithRetry("TestUser")
+            .assertIsUsersCreated(user1)
+            .assertIsUsersCreated(user2);
+
+        log.info("Step 1: Login with user1 and create Plain Text document in MyFiles");
+        UserModel testuser1 = new UserModel(user1, password);
+        authenticateUsingLoginPage(testuser1);
+
+        myFilesPage
+            .navigate();
+        myFilesPage
+            .click_CreateButton()
+            .click_CreateFromTemplateOption(CreateMenuOption.PLAIN_TEXT);
+        create
+            .typeName(C7648name)
+            .typeContent(C7648content)
+            .typeTitle(C7648title)
+            .typeDescription(C7648description)
+            .clickCreate();
+        myFilesPage
+            .navigate();
+        Assert.assertTrue(myFilesPage.isFileNameDisplayed(C7648name), "C7648 name is not displayed in My Files");
+
+        log.info("Step 2: Login with user2 and check that the file create by user1 is not visible in My Files");
+        UserModel testuser2 = new UserModel(user2, password);
+        authenticateUsingLoginPage(testuser2);
+
+        myFilesPage
+            .navigate();
+        uploadContent
+            .uploadContent(testFilePath);
+        myFilesPage
+            .navigate();
+        Assert.assertFalse(myFilesPage.isFileNameDisplayed(C7648name),"C7648 name is displayed in My Files");
+
+        deleteUsersIfNotNull(testuser1);
+        deleteUsersIfNotNull(testuser2);
     }
 
     @TestRail (id = "C7658")
     @Test (groups = { TestGroup.SANITY, TestGroup.CONTENT })
-    public void verifyPresenceOfMyFilesInHeaderBar()
-    {
-        setupAuthenticatedSession(user, password);
-        userDashboard.navigate(siteName);
+    public void verifyPresenceOfMyFilesInHeaderBar() {
+        log.info("PreCondition: Creating a TestUser");
+        user.set(getDataUser().usingAdmin().createRandomTestUser());
+        getCmisApi().authenticateUser(getAdminUser());
+        authenticateUsingCookies(user.get());
 
-        LOG.info("Step 1: Check that the My Files link is available in the toolbar");
-        toolbar.assertMyFilesIsDisplayed();
+        log.info("Step 1: Check that the My Files link is available in the toolbar");
+        userDashboard
+            .navigate(siteName);
+        toolbar
+            .assertMyFilesIsDisplayed();
 
-        LOG.info("Step 2: Access the My Files via link in toolbar");
-        toolbar.clickMyFiles();
-//        Assert.assertEquals(myFilesPage.getPageTitle(), "Alfresco » My Files", "User is not redirected to the repository page");
+        log.info("Step 2: Access the My Files via link in toolbar");
+        toolbar
+            .clickMyFiles();
+        myFilesPage
+            .assertBrowserPageTitleIs("Alfresco » My Files");
     }
 }
