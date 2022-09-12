ede233b39c7fa750772a0a7b1cfe31ff2edbc211
ggnanadhas <godwin.gnanadhas@globallogic.com>
2022-09-12 19:29:01 +0530

    Test Class Refactored

--- alfresco-tas-share-test/src/test/java/org/alfresco/share/alfrescoContent/workingWithFilesOutsideTheLibrary/repository/ActionsManageAspectsTests.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/share/alfrescoContent/workingWithFilesOutsideTheLibrary/repository/ActionsManageAspectsTests.java
@@ -1,128 +1,159 @@
 package org.alfresco.share.alfrescoContent.workingWithFilesOutsideTheLibrary.repository;
 
+import lombok.extern.slf4j.Slf4j;
+import org.alfresco.po.share.DeleteDialog;
 import org.alfresco.po.share.alfrescoContent.RepositoryPage;
 import org.alfresco.po.share.alfrescoContent.aspects.AspectsForm;
 import org.alfresco.po.share.site.ItemActions;
-import org.alfresco.share.ContextAwareWebTest;
+import org.alfresco.share.BaseTest;
 import org.alfresco.testrail.TestRail;
-import org.alfresco.utility.data.RandomData;
+import org.alfresco.utility.model.FolderModel;
 import org.alfresco.utility.model.TestGroup;
+import org.alfresco.utility.model.UserModel;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class ActionsManageAspectsTests extends ContextAwareWebTest
+@Slf4j
+public class ActionsManageAspectsTests extends BaseTest
 {
-    //Autowired
     private RepositoryPage repositoryPage;
-
-    //@Autowired
+    private DeleteDialog deleteDialog;
+    private UserModel testUser1;
+    private FolderModel testFolder;
+    private FolderModel testFolderC13763;
+    private FolderModel testFolderC13764;
     private AspectsForm aspectsForm;
+    private static final String classifiableAspect = "Classifiable";
 
-    private String nonAdminUser;
-    private String folderInRepoUserHomes;
-    private String folderInRepoMainPath;
-    private String repositoryUserHomesPath;
+    private final ThreadLocal<UserModel> user = new ThreadLocal<>();
 
-    @BeforeClass (alwaysRun = true)
-    public void setupTest()
-    {
-        nonAdminUser = String.format("nonAdminUser%s", RandomData.getRandomAlphanumeric());
-        folderInRepoMainPath = String.format("testFolderInRepoMainPath%s", RandomData.getRandomAlphanumeric());
-        folderInRepoUserHomes = String.format("folderInRepoUserHomes%s", RandomData.getRandomAlphanumeric());
-        userService.create(adminUser, adminPassword, nonAdminUser, password, nonAdminUser + domain, nonAdminUser, nonAdminUser);
-        contentService.createFolderInRepository(adminUser, adminPassword, folderInRepoMainPath, "/");
-        contentService.createFolderInRepository(nonAdminUser, password, folderInRepoUserHomes, "User Homes/" + nonAdminUser);
+    @BeforeMethod(alwaysRun = true)
+    public void setupTest() throws Exception {
+        repositoryPage = new RepositoryPage(webDriver);
+        deleteDialog = new DeleteDialog(webDriver);
+        aspectsForm = new AspectsForm(webDriver);
+
+        log.info("PreCondition1: Any test user is created");
+        testUser1 = dataUser.usingAdmin().createRandomTestUser();
+        getCmisApi().authenticateUser(getAdminUser());
+
+        log.info("Create a Folder in Admin Repository-> User Homes ");
+        authenticateUsingLoginPage(getAdminUser());
+
+        testFolder = FolderModel.getRandomFolderModel();
+        getCmisApi().usingAdmin().usingUserHome(testUser1.getUsername()).createFolder(testFolder).assertThat().existsInRepo();
+
     }
 
-    @AfterClass (alwaysRun = true)
+    @AfterMethod(alwaysRun = true)
     public void cleanup()
     {
-        userService.delete(adminUser, adminPassword, nonAdminUser);
-        contentService.deleteTreeByPath(adminUser, adminPassword, folderInRepoMainPath);
-        contentService.deleteTreeByPath(adminUser, adminPassword, "/User Homes/" + nonAdminUser);
+        deleteUsersIfNotNull(user.get());
+
+        log.info("Delete the Created Folder from Admin Repository-> User Homes");
+        authenticateUsingLoginPage(getAdminUser());
+        repositoryPage
+            .navigateByMenuBar()
+            .click_FolderName("User Homes")
+            .select_ItemsAction(testUser1.getUsername(),ItemActions.DELETE_FOLDER);
+        deleteDialog
+            .confirmDeletion();
 
     }
 
     @TestRail (id = "C8254")
-    @Test (groups = { TestGroup.SANITY, TestGroup.CONTENT, "tobefixed" })
+    @Test(groups = { TestGroup.SANITY, TestGroup.CONTENT })
     public void checkAspectsForm()
     {
-        setupAuthenticatedSession(nonAdminUser, password);
-        repositoryPage.navigate();
-        repositoryPage.clickFolderFromExplorerPanel("User Homes");
-        repositoryPage.clickOnFolderName(nonAdminUser);
+        authenticateUsingLoginPage(testUser1);
+        repositoryPage
+            .navigateByMenuBar()
+            .click_FolderName("User Homes")
+            .clickOnFolderName(testUser1.getUsername())
+            .assertFileIsDisplayed(testFolder.getName());
 
-        LOG.info("Step1: Click 'More'->'Manage Aspects' action for created folder and verify the Manage Aspects form");
-        repositoryPage.selectItemAction(folderInRepoUserHomes, ItemActions.MANAGE_ASPECTS);
-        Assert.assertTrue(aspectsForm.isAspectsFormTitleDisplayed(), "Aspects for the file form is not diplayed");
-        Assert.assertTrue(aspectsForm.isAvailableToAddPanelDisplayed(), "Available to Add panel is not diaplyed");
-        Assert.assertTrue(aspectsForm.isCurrentlySelectedPanel(), "Currently Selected panel is not diaplyed");
+        log.info("Step1: Click 'More'->'Manage Aspects' action for created folder and verify the Manage Aspects form");
+        repositoryPage
+            .select_ItemsAction(testFolder.getName(), ItemActions.MANAGE_ASPECTS);
+        Assert.assertTrue(aspectsForm.isAspectsFormTitleDisplayed(), "Aspects for the file form is not displayed");
+        Assert.assertTrue(aspectsForm.isAvailableToAddPanelDisplayed(), "Available to Add panel is not displayed");
+        Assert.assertTrue(aspectsForm.isCurrentlySelectedPanel(), "Currently Selected panel is not displayed");
         Assert.assertTrue(aspectsForm.areAddButtonsDisplayed(), "Add buttons are not displayed for all the available to add aspects");
         Assert.assertTrue(aspectsForm.areRemoveButtonsDisplayed(), "Remove buttons are not displayed for all the selected aspects");
-        Assert.assertTrue(aspectsForm.isSaveButtonDisplayed(), "Apply Chnages button is not displayed");
+        Assert.assertTrue(aspectsForm.isSaveButtonDisplayed(), "Apply Changes button is not displayed");
         Assert.assertTrue(aspectsForm.isCancelButtonDisplayed(), "Cancel button is not displayed");
         Assert.assertTrue(aspectsForm.isCloseButtonDisplayed(), "Close button is not displayed");
-
-        cleanupAuthenticatedSession();
     }
 
     @TestRail (id = "C8250")
-    @Test (groups = { TestGroup.SANITY, TestGroup.CONTENT, "tobefixed" })
+    @Test (groups = { TestGroup.SANITY, TestGroup.CONTENT })
     public void manageAspectsApplyChanges()
     {
-        setupAuthenticatedSession(nonAdminUser, password);
-        repositoryPage.navigate();
-        repositoryPage.clickFolderFromExplorerPanel("User Homes");
-        repositoryPage.clickOnFolderName(nonAdminUser);
+        authenticateUsingLoginPage(testUser1);
+        repositoryPage
+            .navigateByMenuBar()
+            .click_FolderName("User Homes")
+            .clickOnFolderName(testUser1.getUsername());
 
-        LOG.info("Step1: Click 'More'->'Manage Aspects' action for the created folder");
-        repositoryPage.selectItemAction(folderInRepoUserHomes, ItemActions.MANAGE_ASPECTS);
+        log.info("Step1: Click 'More'->'Manage Aspects' action for the created folder");
+        repositoryPage
+            .selectItemAction(testFolder.getName(), ItemActions.MANAGE_ASPECTS);
 
-        LOG.info("Step2: From 'Available to Add' list, click 'Add' icon next to an aspect and verify it's displayed in 'Currently Selected' list");
-        aspectsForm.addAspect("Classifiable");
-        Assert.assertTrue(aspectsForm.isAspectPresentOnCurrentlySelectedList("Classifiable"), "Aspect is not added to 'Currently Selected' list");
-        Assert.assertFalse(aspectsForm.isAspectPresentOnAvailableAspectList("Classifiable"), "Aspect is present on 'Available to Add' list");
+        log.info("Step2: From 'Available to Add' list, click 'Add' icon next to an aspect and verify it's displayed in 'Currently Selected' list");
+        aspectsForm
+            .assertAspactPresentInAvailableList(classifiableAspect)
+            .addAspect(classifiableAspect);
+        aspectsForm
+            .assertAspactPresentInCurrentlySelectedList(classifiableAspect);
 
-        LOG.info("Step3: Click 'Apply Changes' and verify the aspect is added");
-        aspectsForm.clickApplyChangesButton();
-        repositoryPage.selectItemAction(folderInRepoUserHomes, ItemActions.MANAGE_ASPECTS);
-        Assert.assertTrue(aspectsForm.isAspectPresentOnCurrentlySelectedList("Classifiable"), "Aspect is not added to 'Currently Selected' list");
-        Assert.assertFalse(aspectsForm.isAspectPresentOnAvailableAspectList("Classifiable"), "Aspect is present on 'Available to Add' list");
-
-        cleanupAuthenticatedSession();
+        log.info("Step3: Click 'Apply Changes' and verify the aspect is added");
+        aspectsForm
+            .clickApplyChangesButton();
+        repositoryPage
+            .selectItemAction(testFolder.getName(), ItemActions.MANAGE_ASPECTS);
+        aspectsForm
+            .assertAspactPresentInCurrentlySelectedList(classifiableAspect);
     }
 
     @TestRail (id = "C13763")
     @Test (groups = { TestGroup.SANITY, TestGroup.CONTENT })
-    public void manageAspectsForNonAdminOnRepositoryMainFolder()
-    {
-        setupAuthenticatedSession(nonAdminUser, password);
-        repositoryPage.navigate();
-        LOG.info("Step1: Click 'More'->'Manage Aspects' action for the created folder");
-        repositoryPage.mouseOverContentItem(folderInRepoMainPath);
-        Assert.assertEquals(repositoryPage.isMoreMenuDisplayed(folderInRepoMainPath), false);
-        Assert.assertEquals(repositoryPage.isActionAvailableForLibraryItem(folderInRepoMainPath, ItemActions.MANAGE_ASPECTS), false);
+    public void manageAspectsForNonAdminOnRepositoryMainFolder() throws Exception {
 
-        cleanupAuthenticatedSession();
+        log.info("PreCondition: Create a Folder in Repository & Login with Non Admin User - Navigate to Repository Page");
+        testFolderC13763 = FolderModel.getRandomFolderModel();
+        getCmisApi().usingRoot().createFolder(testFolderC13763).assertThat().existsInRepo();
+
+        authenticateUsingLoginPage(testUser1);
+        repositoryPage.navigateByMenuBar();
+
+        log.info("Step1: Click 'More'->'Manage Aspects' action for the created folder");
+        repositoryPage
+            .mouseOverContentItem(testFolderC13763.getName());
+        repositoryPage
+            .assertIsMoreMenuNotDisplayed(testFolderC13763.getName())
+            .assertActionItem_Not_AvailableInTheRepositoryLibraryItems(testFolderC13763.getName(),ItemActions.MANAGE_ASPECTS);
     }
 
     @TestRail (id = "C13764")
     @Test (groups = { TestGroup.SANITY, TestGroup.CONTENT })
-    public void manageAspectsAdminOnRepositoryMainFolder()
-    {
-        setupAuthenticatedSession(adminUser, adminPassword);
-        repositoryPage.navigate();
+    public void manageAspectsAdminOnRepositoryMainFolder() throws Exception {
 
-        LOG.info("Step1: Click 'More'->'Manage Aspects' action for the created folder");
-        Assert.assertTrue(repositoryPage.getFoldersList().contains(folderInRepoMainPath), "Folder is not displayed!");
+        log.info("PreCondition: Admin Create a Folder in Repository & Navigate to Repository Page");
+        testFolderC13764 = FolderModel.getRandomFolderModel();
+        getCmisApi().usingRoot().createFolder(testFolderC13764).assertThat().existsInRepo();
+        repositoryPage.navigateByMenuBar();
 
-        repositoryPage.mouseOverContentItem(folderInRepoMainPath);
-        Assert.assertTrue(repositoryPage.isMoreMenuDisplayed(folderInRepoMainPath), "More menu is not displayed");
-        Assert.assertTrue(repositoryPage.isActionAvailableForLibraryItem(folderInRepoMainPath, ItemActions.MANAGE_ASPECTS), "'Manage Aspects' action is not available.");
+        log.info("Step1: Click 'More'->'Manage Aspects' action for the created folder");
+        repositoryPage
+            .assertFileIsDisplayed(testFolderC13764.getName());
+        repositoryPage
+            .mouseOverContentItem(testFolderC13764.getName());
+        repositoryPage
+            .assertIsMoreMenuDisplayed(testFolderC13764.getName())
+            .assertActionItem_AvailableInTheRepositoryLibraryItems(testFolderC13764.getName(), ItemActions.MANAGE_ASPECTS);
 
-        cleanupAuthenticatedSession();
     }
 
 }
