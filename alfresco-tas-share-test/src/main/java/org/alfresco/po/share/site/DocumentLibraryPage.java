2ae383e99855e44a36053e175d68f78e70d7274b
preddy <Prakash.Reddy@contractors.onbase.com>
2022-10-04 18:29:06 +0530

    Test class refactored APPS-1712
    Refactored class:
    TagTests.java

--- alfresco-tas-share-test/src/main/java/org/alfresco/po/share/site/DocumentLibraryPage.java
+++ alfresco-tas-share-test/src/main/java/org/alfresco/po/share/site/DocumentLibraryPage.java
@@ -1067,12 +1067,22 @@ public class DocumentLibraryPage extends SiteCommon<DocumentLibraryPage> // TODO
      */
     public void mouseOverNoTags(String contentName)
     {
+        waitInSeconds(2);
         WebElement contentElement = selectDocumentLibraryItemRow(contentName);
         Parameter.checkIsMandotary("Content selector", contentElement);
         WebElement tagElement = contentElement.findElement(noTagsSelector);
         Parameter.checkIsMandotary("Tag selector", tagElement);
         mouseOver(tagElement);
         waitUntilElementIsVisible(contentElement.findElement(editTagSelector));
+    }
+    public void mouseOverNoTagsWithNoEditIcon(String contentName)
+    {
+        waitInSeconds(2);
+        WebElement contentElement = selectDocumentLibraryItemRow(contentName);
+        Parameter.checkIsMandotary("Content selector", contentElement);
+        WebElement tagElement = contentElement.findElement(noTagsSelector);
+        Parameter.checkIsMandotary("Tag selector", tagElement);
+        mouseOver(tagElement);
     }
 
     public boolean isEditTagInputFieldDisplayed()
@@ -1597,6 +1607,12 @@ public class DocumentLibraryPage extends SiteCommon<DocumentLibraryPage> // TODO
     {
         log.info("Verify that the Edit Tag Icon is displayed");
         assertTrue(isEditTagIconDisplayed(contentName), "Edit Tag icon is not displayed");
+        return this;
+    }
+    public DocumentLibraryPage assertIsEditTagIconNotDisplayed(String contentName)
+    {
+        log.info("Verify that the Edit Tag Icon is displayed");
+        assertFalse(isEditTagIconDisplayed(contentName), "Edit Tag icon is not displayed");
         return this;
     }
 
