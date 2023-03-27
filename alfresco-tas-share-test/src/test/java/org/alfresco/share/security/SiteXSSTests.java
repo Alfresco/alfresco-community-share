13d6cdae52e551c33c7b2c6b3982c45d19223a4d
ggnanadhas <godwin.gnanadhas@globallogic.com>
2023-03-27 19:04:27 +0530

    Test Class "SiteXSSTests" Refactored

--- alfresco-tas-share-test/src/test/java/org/alfresco/share/security/SiteXSSTests.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/share/security/SiteXSSTests.java
@@ -3,68 +3,74 @@ package org.alfresco.share.security;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.alfresco.common.DataProviderClass;
 import org.alfresco.po.share.site.CreateSiteDialog;
 import org.alfresco.po.share.site.SiteDashboardPage;
 import org.alfresco.po.share.user.UserDashboardPage;
-import org.alfresco.share.ContextAwareWebTest;
+import org.alfresco.share.BaseTest;
 import org.alfresco.testrail.TestRail;
 import org.alfresco.utility.data.RandomData;
 import org.alfresco.utility.model.TestGroup;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.alfresco.utility.model.UserModel;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
-public class SiteXSSTests extends ContextAwareWebTest
+@Slf4j
+public class SiteXSSTests extends BaseTest
 {
-    //@Autowired
     private CreateSiteDialog createSiteDialog;
-
-    //@Autowired
     private SiteDashboardPage siteDashboardPage;
-
-
+    private UserDashboardPage userDashboardPage;
+    private final ThreadLocal<UserModel> user1 = new ThreadLocal<>();
     private String uniqueIdentifier = RandomData.getRandomAlphanumeric();
     private String testUser = "TestUser_" + uniqueIdentifier;
 
-    @BeforeClass (alwaysRun = true)
+    @BeforeMethod(alwaysRun = true)
     private void beforeClass()
     {
-        userService.create(adminUser, adminPassword, testUser, password, testUser + domain, "testUser_firstName", "testUser_lastName");
+        log.info("Precondition 1: Create a new user");
+        user1.set(getDataUser().usingAdmin().createRandomTestUser());
+        getCmisApi().authenticateUser(getAdminUser());
 
-        LOG.info("Precondition 1: Any user logged in Share.");
-        setupAuthenticatedSession(testUser, password);
+        createSiteDialog = new CreateSiteDialog(webDriver);
+        userDashboardPage = new UserDashboardPage(webDriver);
+        siteDashboardPage = new SiteDashboardPage(webDriver);
+
+
+        log.info("Precondition 2: Any user logged in Share.");
+        authenticateUsingLoginPage(user1.get());
     }
 
-    @AfterClass (alwaysRun = true)
+    @AfterMethod(alwaysRun = true)
     private void afterClass()
     {
-        cleanupAuthenticatedSession();
-        userService.delete(adminUser, adminPassword, testUser);
+        deleteUsersIfNotNull(user1.get());
     }
 
 
     @TestRail (id = "C286607")
-    @Test (groups = { TestGroup.SANITY, TestGroup.SECURITY, "xsstests" }, dataProvider = "XSSSecurity", dataProviderClass = DataProviderClass.class)
+    @Test(groups = { TestGroup.SANITY, TestGroup.SECURITY, "xsstests" }, dataProvider = "XSSSecurity", dataProviderClass = DataProviderClass.class)
     public void creatingSiteWithXSSSiteID(String XSSSiteID)
     {
-        LOG.info("STEP 1: Navigate to the Sites drop-down menu and select Create site link;");
+        log.info("STEP 1: Navigate to the Sites drop-down menu and select Create site link;");
         createSiteDialog.navigateByMenuBar();
         assertTrue(createSiteDialog.isSiteIDInputFieldDisplayed(), "'SiteID' field is not displayed.");
 
-        LOG.info("STEP 2: Enter \"test\" in Name field;");
+        log.info("STEP 2: Enter \"test\" in Name field;");
         createSiteDialog.typeInNameInput("test");
         assertEquals(createSiteDialog.getNameInputText(), "test", "Text could not be entered in 'Name' field.");
 
-        LOG.info("STEP 3: Enter into \"Site ID\" field XSS string, try the next cases: '" + XSSSiteID + "'.");
+        log.info("STEP 3: Enter into \"Site ID\" field XSS string, try the next cases: '" + XSSSiteID + "'.");
         createSiteDialog.typeInSiteID(XSSSiteID);
         assertEquals(createSiteDialog.getSiteIdInputText(), XSSSiteID, "Text could not be entered in 'Site ID' field.");
 
-        LOG.info("STEP 4: Check that OK button is disabled;");
+        log.info("STEP 4: Check that OK button is disabled;");
         assertEquals(createSiteDialog.getCreateButtonState(), "true", "'Create Button' is not disabled.");
 
-        LOG.info("Close PopUp.");
+        log.info("Close PopUp.");
         createSiteDialog.clickCloseXButton();
     }
 
@@ -76,27 +82,27 @@ public class SiteXSSTests extends ContextAwareWebTest
         String siteId = "test" + RandomData.getRandomAlphanumeric();
         String expectedRelativePath = "share/page/site/" + XSSName + "/dashboard";
 
-        userDashboard.navigate(testUser);
+        userDashboardPage.navigate(testUser);
 
-        LOG.info("STEP 1: Navigate to the Sites drop-down menu and select Create site link;");
+        log.info("STEP 1: Navigate to the Sites drop-down menu and select Create site link;");
         createSiteDialog.navigateByMenuBar();
         assertTrue(createSiteDialog.isSiteIDInputFieldDisplayed(), "'SiteID' field is not displayed.");
 
-        LOG.info("STEP 2: Enter into \"Name\" field XSS string, try the next cases: '" + XSSName + "'.");
+        log.info("STEP 2: Enter into \"Name\" field XSS string, try the next cases: '" + XSSName + "'.");
         createSiteDialog.typeInNameInput(XSSName);
         assertEquals(createSiteDialog.getNameInputText(), XSSName, "Text could not be entered in 'Name' field.");
 
-        LOG.info("STEP 3.1: Enter \"test\" in 'Site ID' field;");
+        log.info("STEP 3.1: Enter \"test\" in 'Site ID' field;");
         createSiteDialog.typeInSiteID(siteId);
         assertEquals(createSiteDialog.getSiteIdInputText(), siteId, "Text could not be entered in 'SiteID' field.");
 
-        LOG.info("STEP 3.2: Enter 'description' in 'Description' field.");
+        log.info("STEP 3.2: Enter 'description' in 'Description' field.");
         createSiteDialog.typeInDescription("description");
 
-        LOG.info("STEP 3.3: Select visibility to 'public'.");
+        log.info("STEP 3.3: Select visibility to 'public'.");
         createSiteDialog.selectPublicVisibility();
 
-        LOG.info("STEP 4: Press 'Create' button;");
+        log.info("STEP 4: Press 'Create' button;");
         createSiteDialog.clickCreateButton();
         siteDashboardPage.setCurrentSiteName(XSSName);
         assertEquals(siteDashboardPage.getRelativePath(), expectedRelativePath, "User is not redirected to the created site.");
@@ -109,28 +115,28 @@ public class SiteXSSTests extends ContextAwareWebTest
     {
         String siteName = "test" + RandomData.getRandomAlphanumeric();
 
-        userDashboard.navigate(testUser);
+        userDashboardPage.navigate(testUser);
 
-        LOG.info("STEP 1: Navigate to the Sites drop-down menu and select 'Create site' link;");
+        log.info("STEP 1: Navigate to the Sites drop-down menu and select 'Create site' link;");
         createSiteDialog.navigateByMenuBar();
         assertTrue(createSiteDialog.isDescriptionInputFieldDisplayed(), "'Description' field is not displayed.");
 
-        LOG.info("STEP 2: Enter " + siteName + " in 'Name' field;");
+        log.info("STEP 2: Enter " + siteName + " in 'Name' field;");
         createSiteDialog.typeInNameInput(siteName);
         assertEquals(createSiteDialog.getNameInputText(), siteName, "Text '" + siteName + "' could not be entered in 'Name' field.");
 
-        LOG.info("STEP 3: Enter " + siteName + " in 'Site ID' field;");
+        log.info("STEP 3: Enter " + siteName + " in 'Site ID' field;");
         createSiteDialog.typeInSiteID(siteName);
         assertEquals(createSiteDialog.getSiteIdInputText(), siteName, "Text could not be entered in 'SiteID' field.");
 
-        LOG.info("STEP 4: Select visibility to 'public'.");
+        log.info("STEP 4: Select visibility to 'public'.");
         createSiteDialog.selectPublicVisibility();
 
-        LOG.info("STEP 5: Enter XSS text in 'Description' field , try the next cases: '" + XSSDescription + "'.");
+        log.info("STEP 5: Enter XSS text in 'Description' field , try the next cases: '" + XSSDescription + "'.");
         createSiteDialog.typeInDescription(XSSDescription);
         assertEquals(createSiteDialog.getDescriptionInputText(), XSSDescription, "Text '" + XSSDescription + "' could not be entered in 'Description' field.");
 
-        LOG.info("STEP 4: Press 'Create' button;");
+        log.info("STEP 4: Press 'Create' button;");
         createSiteDialog.clickCreateButton();
         siteDashboardPage.setCurrentSiteName(siteName);
     }
