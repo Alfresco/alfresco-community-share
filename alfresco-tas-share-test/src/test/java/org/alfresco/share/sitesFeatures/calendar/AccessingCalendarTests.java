f318e195c6bfcbadf500cb89ba7eb3e7914aa4f1
rajsharma <Rajat.Sharma@contractors.onbase.com>
2023-08-29 10:58:50 +0530

    Testcases refactor for APPS-2118

--- alfresco-tas-share-test/src/test/java/org/alfresco/share/sitesFeatures/calendar/AccessingCalendarTests.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/share/sitesFeatures/calendar/AccessingCalendarTests.java
@@ -1,30 +1,36 @@
 package org.alfresco.share.sitesFeatures.calendar;
 
+import static org.alfresco.utility.web.AbstractWebTest.getBrowser;
 import static org.testng.Assert.assertEquals;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import lombok.extern.slf4j.Slf4j;
 import org.alfresco.dataprep.DashboardCustomization.Page;
 import org.alfresco.dataprep.SiteService;
+import org.alfresco.po.share.searching.SearchPage;
 import org.alfresco.po.share.site.CustomizeSitePage;
 import org.alfresco.po.share.site.SiteDashboardPage;
 import org.alfresco.po.share.site.SitePageType;
 import org.alfresco.po.share.site.calendar.CalendarPage;
+import org.alfresco.po.share.toolbar.Toolbar;
+import org.alfresco.share.BaseTest;
 import org.alfresco.share.ContextAwareWebTest;
 import org.alfresco.testrail.TestRail;
 import org.alfresco.utility.data.RandomData;
+import org.alfresco.utility.model.SiteModel;
 import org.alfresco.utility.model.TestGroup;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.alfresco.utility.model.UserModel;
+import org.testng.annotations.*;
 
 /// **
 // * Created by Gabriela Virna on 7/07/2016.
 // */
 //
-public class AccessingCalendarTests extends ContextAwareWebTest
+@Slf4j
+public class AccessingCalendarTests extends BaseTest
 {
     //@Autowired
     CalendarPage calendarPage;
@@ -32,25 +38,43 @@ public class AccessingCalendarTests extends ContextAwareWebTest
     CustomizeSitePage customizeSitePage;
     //@Autowired
     SiteDashboardPage siteDashboardPage;
+    //@Autowired
+    Toolbar toolbar;
 
-    private String user = String.format("User1%s", RandomData.getRandomAlphanumeric());
-    private String siteName = String.format("Site1%s", RandomData.getRandomAlphanumeric());
+    //@Autowired
+    SearchPage searchPage;
+    private final ThreadLocal<UserModel> user1 = new ThreadLocal<>();
+    private final ThreadLocal<UserModel> user2 = new ThreadLocal<>();
+    private final ThreadLocal<SiteModel> siteName = new ThreadLocal<>();
 
-    @BeforeClass (alwaysRun = true)
+    @BeforeMethod(alwaysRun = true)
     public void setupTest()
     {
-        userService.create(adminUser, adminPassword, user, password, user + domain, "firstName", "lastName");
-        siteService.create(user, password, domain, siteName, "description", SiteService.Visibility.PUBLIC);
-        siteService.addPageToSite(user, password, siteName, Page.CALENDAR, null);
-        setupAuthenticatedSession(user, password);
+        log.info("PreCondition: Creating a TestUser1");
+        user1.set(getDataUser().usingAdmin().createRandomTestUser());
+        getCmisApi().authenticateUser(getAdminUser());
+
+        log.info("PreCondition: Creating a TestUser2");
+        user2.set(getDataUser().usingAdmin().createRandomTestUser());
+        getCmisApi().authenticateUser(getAdminUser());
+
+        log.info("PreCondition: Creating a Random Site");
+        siteName.set(getDataSite().usingUser(user1.get()).createPublicRandomSite());
+        getCmisApi().authenticateUser(user1.get());
+
+        toolbar = new Toolbar(webDriver);
+        searchPage = new SearchPage(webDriver);
+        siteDashboardPage = new SiteDashboardPage(webDriver);
     }
 
-    @AfterClass (alwaysRun = true)
+
+    @AfterMethod (alwaysRun = true)
     public void cleanup()
     {
-        userService.delete(adminUser, adminPassword, user);
-        contentService.deleteTreeByPath(adminUser, adminPassword, "/User Homes/" + user);
-        siteService.delete(adminUser, adminPassword, siteName);
+        deleteUsersIfNotNull(user1.get());
+        deleteUsersIfNotNull(user2.get());
+        deleteSitesIfNotNull(siteName.get());
+        deleteAllCookiesIfNotNull();
     }
 
     @TestRail (id = "C5437")
@@ -58,22 +82,22 @@ public class AccessingCalendarTests extends ContextAwareWebTest
 
     public void accessTheCalendarPage()
     {
-        LOG.info("Step 1 - Open Site1's dashboard and click on 'Calendar' link.");
-        calendarPage.navigate(siteName);
+        log.info("Step 1 - Open Site1's dashboard and click on 'Calendar' link.");
+        calendarPage.navigate(siteName.get());
         DateFormat df = new SimpleDateFormat("MMMM yyyy");
         assertEquals(calendarPage.getCalendarHeader(), df.format(new Date()), "'Calendar' page, which defaults to the Month view, is opened.");
 
-        LOG.info("Step 2 - Open 'Customize Site' page for " + siteName + " and rename 'Calendar' page to 'newCalendar'.");
-        customizeSitePage.navigate(siteName);
+        log.info("Step 2 - Open 'Customize Site' page for " + siteName + " and rename 'Calendar' page to 'newCalendar'.");
+        customizeSitePage.navigate(siteName.get());
         customizeSitePage.renameSitePage(SitePageType.CALENDER, "newCalendar");
         assertEquals(customizeSitePage.getPageDisplayName(SitePageType.CALENDER), "newCalendar", "Calendar display page name is modified.");
 
-        LOG.info("Step 3 - Click OK to save changes");
+        log.info("Step 3 - Click OK to save changes");
         customizeSitePage.saveChanges();
-        siteDashboardPage.navigate(siteName);
+        siteDashboardPage.navigate(siteName.get());
         assertEquals(siteDashboardPage.getPageDisplayName(SitePageType.CALENDER), "newCalendar", "Calendar display page name is modified.");
 
-        LOG.info("Step 4 - Click on 'newCalendar' link.");
+        log.info("Step 4 - Click on 'newCalendar' link.");
         siteDashboardPage.clickLinkFromHeaderNavigationMenu(SitePageType.CALENDER);
         assertEquals(getBrowser().getTitle(), "Alfresco » newCalendar", "Calendar page is opened.");
     }
