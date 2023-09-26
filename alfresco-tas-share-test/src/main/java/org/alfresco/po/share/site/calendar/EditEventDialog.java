efa14274dbeb1ca64d56e8f715d174a740022954
rajsharma <Rajat.Sharma@contractors.onbase.com>
2023-09-26 19:44:49 +0530

    Testcases refactor for APPS-2207

--- alfresco-tas-share-test/src/main/java/org/alfresco/po/share/site/calendar/EditEventDialog.java
+++ alfresco-tas-share-test/src/main/java/org/alfresco/po/share/site/calendar/EditEventDialog.java
@@ -1,15 +1,119 @@
 package org.alfresco.po.share.site.calendar;
 
+import lombok.extern.slf4j.Slf4j;
+import org.alfresco.po.share.BaseDialogComponent;
 import org.alfresco.utility.web.annotation.PageObject;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import ru.yandex.qatools.htmlelements.element.Button;
+import ru.yandex.qatools.htmlelements.element.TextInput;
 
 /**
  * Created by Claudia Agache on 7/20/2016.
  */
-@PageObject
-public class EditEventDialog extends AddEventDialog
+@Slf4j
+public class EditEventDialog extends AddEventDialogPage
 {
+
+
+    private final By eventInformationPanels = By.id("eventInfoPanel_h");
+    private final By dialogHeader = By.id("eventEditPanel-dialog_h");
+
+    private final By eventLocation = By.id("eventEditPanel-location");
+
+    private final By editButton = By.cssSelector("button[id$='edit-button-button']");
+
+    private final By eventDescription = By.id("eventEditPanel-description");
+
+    private final By currentTagList = By.cssSelector("#eventEditPanel-current-tags .taglibrary-action>span");
+
+    private final By tagsDetails = By.xpath("//div[contains(text(),'Tags:')]/following-sibling::div");
+
+    private final By descriptionDetails = By.xpath("//div[contains(text(),'Description:')]/following-sibling::div");
+
+    private final By whereDetails = By.xpath("//div[contains(text(),'Where:')]/following-sibling::div");
+
+    private final By whatDetails = By.xpath("//div[contains(text(),'What:')]/following-sibling::div");
+
+    private final By endDateTime = By.cssSelector("div[id$='_defaultContainer-enddate']");
+
+    private final By startDateTime = By.cssSelector("div[id$='_defaultContainer-startdate']");
+
+    private final By cancelButton = By.cssSelector("button[id$='_defaultContainer-cancel-button-button']");
+
+
+    public EditEventDialog(ThreadLocal<WebDriver> webDriver) {
+        super(webDriver);
+    }
+
     public Boolean isDialogDisplayed()
     {
-        return browser.isElementDisplayed(dialogHeader) && dialogHeader.getText().equals("Edit Event");
+        waitInSeconds(3);
+        return isElementDisplayed(dialogHeader) && findElement(dialogHeader).getText().equals("Edit Event");
     }
+
+    public Boolean isEventInformationPanelDisplayed()
+    {
+        waitInSeconds(3);
+        return isElementDisplayed(eventInformationPanels) && findElement(eventInformationPanels).getText().equals("Event Information");
+    }
+
+    public String getEventLocation()
+    {
+        return findElement(eventLocation).getAttribute("value");
+    }
+
+    public void clickEditButton()
+    {
+        findElement(editButton).click();
+    }
+
+    public String getEventDescription()
+    {
+        return findElement(eventDescription).getAttribute("value");
+    }
+
+    public void removeTag(String tag) {
+        findElement(currentTagList).click();
+    }
+
+    public String getStartDateTime()
+    {
+        return findElement(startDateTime).getText();
+    }
+
+
+    public String getWhatDetails()
+    {
+        waitInSeconds(3);
+        return findElement(whatDetails).getText();
+    }
+
+    public String getWhereDetails()
+    {
+        return findElement(whereDetails).getText();
+    }
+
+    public String getDescriptionDetails()
+    {
+        return findElement(descriptionDetails).getText();
+    }
+
+    public String getTagsDetails()
+    {
+        return findElement(tagsDetails).getText().replaceAll("  ", " ");
+    }
+
+    public String getEndDateTime()
+    {
+        return findElement(endDateTime).getText();
+    }
+
+    public void clickOnCancelButton()
+    {
+        findElement(cancelButton).click();
+    }
+
 }
