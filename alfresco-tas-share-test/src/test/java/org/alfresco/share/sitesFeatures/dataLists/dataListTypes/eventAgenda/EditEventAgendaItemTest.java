30b6a865b84835094cd85f86f33297f877bf7055
ggnanadhas <godwin.gnanadhas@globallogic.com>
2023-10-11 10:24:50 +0530

    Test package "dataListTypes" refactored

--- alfresco-tas-share-test/src/test/java/org/alfresco/share/sitesFeatures/dataLists/dataListTypes/eventAgenda/EditEventAgendaItemTest.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/share/sitesFeatures/dataLists/dataListTypes/eventAgenda/EditEventAgendaItemTest.java
@@ -6,27 +6,43 @@ import static org.testng.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
 import org.alfresco.dataprep.DashboardCustomization.Page;
 import org.alfresco.dataprep.DataListsService;
 import org.alfresco.dataprep.SiteService;
+
 import org.alfresco.po.share.site.dataLists.CreateNewItemPopUp.EventAgendaFields;
 import org.alfresco.po.share.site.dataLists.DataListsPage;
 import org.alfresco.po.share.site.dataLists.EditItemPopUp;
-import org.alfresco.share.ContextAwareWebTest;
+
+import org.alfresco.share.BaseTest;
 import org.alfresco.testrail.TestRail;
+
 import org.alfresco.utility.data.RandomData;
+import org.alfresco.utility.model.SiteModel;
 import org.alfresco.utility.model.TestGroup;
+import org.alfresco.utility.model.UserModel;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+@Slf4j
 /**
  * @author Laura.Capsa
  */
-public class EditEventAgendaItemTest extends ContextAwareWebTest
+public class EditEventAgendaItemTest extends BaseTest
 {
+    @Autowired
+    protected SiteService siteService;
+
+    @Autowired
+    protected DataListsService dataListsService;
+
     //@Autowired
     DataListsPage dataListsPage;
 
@@ -34,61 +50,65 @@ public class EditEventAgendaItemTest extends ContextAwareWebTest
     EditItemPopUp editItemPopUp;
 
     String random = RandomData.getRandomAlphanumeric();
-    String userName = "User-" + random;
-    String siteName = "SiteName-" + random;
     String listName = "eventAgenda-" + random;
     String itemReference = "reference-" + random;
     String itemSessionName = "Event agenda item session name";
     String file = "testFile1";
     String fileToAttach = "testDoc.txt";
-
     String newItemReference = "Updated Reference";
     String startTime = "12/11/2016";
-    String updatedItemStartTime = "Sat 12 Nov 2016 ";
     String endTime = "10/12/2016";
-    String updatedItemEndTime = "Sat 10 Dec 2016";
     String newItemSessionName = "Updated Session Name";
     String newItemPresenter = "Update Presenter";
     String newItemAudience = "Updated Audience";
     String newItemNotes = "Updated Notes";
+    private final ThreadLocal<UserModel> userName = new ThreadLocal<>();
+    private final ThreadLocal<SiteModel> siteName = new ThreadLocal<>();
 
-    @BeforeClass (alwaysRun = false)
+    @BeforeMethod (alwaysRun = true)
     public void setupTest()
     {
-        userService.create(adminUser, adminPassword, userName, password, userName + domain, userName, userName);
-        siteService.create(userName, password, domain, siteName, siteName, SiteService.Visibility.PUBLIC);
-        siteService.addPageToSite(userName, password, siteName, Page.DATALISTS, null);
-        dataListsService.createDataList(adminUser, adminPassword, siteName, DataListsService.DataList.EVENT_AGENDA, listName, "Event Agenda list description.");
+        log.info("Precondition: Any Test user is created");
+        userName.set(getDataUser().usingAdmin().createRandomTestUser());
+        getCmisApi().authenticateUser(getAdminUser());
 
-        contentService.uploadFileInSite(userName, password, siteName, testDataFolder + file);
-        contentService.uploadFileInSite(userName, password, siteName, testDataFolder + fileToAttach);
-        dataListsService.addEventAgendaItem(userName, password, siteName, listName, itemReference, null, null, itemSessionName, null, null, null,
+        log.info("PreCondition: Site siteName is created");
+        siteName.set(getDataSite().usingUser(userName.get()).createPublicRandomSite());
+        getCmisApi().authenticateUser(userName.get());
+
+        dataListsPage = new DataListsPage(webDriver);
+        editItemPopUp = new EditItemPopUp(webDriver);
+
+        siteService.addPageToSite(userName.get().getUsername(), userName.get().getPassword(), siteName.get().getId(), Page.DATALISTS, null);
+        dataListsService.createDataList(userName.get().getUsername(), userName.get().getPassword(), siteName.get().getId(), DataListsService.DataList.EVENT_AGENDA, listName, "Event Agenda list description.");
+
+        contentService.uploadFileInSite(userName.get().getUsername(), userName.get().getPassword(), siteName.get().getId(), testDataFolder + file);
+        contentService.uploadFileInSite(userName.get().getUsername(), userName.get().getPassword(), siteName.get().getId(), testDataFolder + fileToAttach);
+        dataListsService.addEventAgendaItem(userName.get().getUsername(), userName.get().getPassword(), siteName.get().getId(), listName, itemReference, null, null, itemSessionName, null, null, null,
             Collections.singletonList(file));
 
-        setupAuthenticatedSession(userName, password);
-        dataListsPage.navigate(siteName);
+        authenticateUsingLoginPage(userName.get());
+
+        dataListsPage.navigate(siteName.get());
         dataListsPage.clickEventAgendaListItem(listName);
     }
 
-    @AfterClass (alwaysRun = false)
+    @AfterMethod (alwaysRun = true)
     public void cleanup()
     {
-        userService.delete(adminUser, adminPassword, userName);
-        contentService.deleteTreeByPath(adminUser, adminPassword, "/User Homes/" + userName);
-        siteService.delete(adminUser, adminPassword, siteName);
+        contentService.deleteTreeByPath(getAdminUser().getUsername(), getAdminUser().getPassword(), "/User Homes/" + userName.get().getUsername());
+        deleteSitesIfNotNull(siteName.get());
+        deleteUsersIfNotNull(userName.get());
 
     }
 
     @TestRail (id = "C10537")
-    @Test (groups = { TestGroup.SANITY, TestGroup.SITES_FEATURES, "tobefixed" })
-    public void verifyPossibilityToEditItem()
-    {
-        LOG.info("STEP1: Click Edit icon for the item");
-        // dataListsPage.currentContent.editItem(Arrays.asList(itemReference, null, null, itemSessionName, null, null, null,
-        // file));
+    @Test (groups = { TestGroup.SANITY, TestGroup.SITES_FEATURES })
+    public void verifyPossibilityToEditItem() {
+        log.info("STEP1: Click Edit icon for the item");
         dataListsPage.clickEditButtonForListItem();
 
-        LOG.info("STEP2: Provide new input for the Reference field");
+        log.info("STEP2: Provide new input for the Reference field");
         editItemPopUp.editContent(EventAgendaFields.Reference.toString(), newItemReference);
         editItemPopUp.editContent(EventAgendaFields.StartTime.toString(), startTime);
         editItemPopUp.editContent(EventAgendaFields.EndTime.toString(), endTime);
@@ -97,14 +117,21 @@ public class EditEventAgendaItemTest extends ContextAwareWebTest
         editItemPopUp.editContent(EventAgendaFields.Audience.toString(), newItemAudience);
         editItemPopUp.editContent(EventAgendaFields.Notes.toString(), newItemNotes);
 
-        LOG.info("STEP3: Click Attachments/Select button and select doc. Click on the + button to add the attachment and then Ok to confirm");
-        //editItemPopUp.removeAttachments(file);
+        log.info("STEP3: Click Attachments/Select button and select doc. Click on the + button to add the attachment and then Ok to confirm");
+        editItemPopUp.removeAttachments(file);
+        editItemPopUp.clickSave();
+
+        dataListsPage.clickEditButtonForListItem();
         editItemPopUp.addAttachmentFromDocumentLibrary(fileToAttach);
 
-        LOG.info("STEP4: Click on Save button");
+        log.info("STEP4: Click on Save button");
         editItemPopUp.clickSave();
-        assertEquals(dataListsPage.currentContent.messageDisplayed(), "Data Item updated successfully", "Edited Data Item message");
-        assertTrue(dataListsPage.currentContent.isListItemDisplayed(Arrays.asList(newItemReference, updatedItemStartTime, updatedItemEndTime, newItemSessionName,
-            newItemPresenter, newItemAudience, newItemNotes, fileToAttach)), "Event agenda item " + newItemReference + " - Data updated");
+        assertEquals(dataListsPage.messageDisplayed(), "Data Item updated successfully", "Edited Data Item message");
+        List<String> expectedList = Arrays.asList(newItemReference, startTime, endTime, newItemSessionName,
+            newItemPresenter, newItemAudience, newItemNotes, fileToAttach);
+        for (String anExpectedList : expectedList)
+        {
+            assertTrue(dataListsPage.getFilterTypeList().contains(anExpectedList), "Data list item is updated.");
+        }
     }
 }
--- alfresco-tas-share-test/src/test/java/org/alfresco/share/sitesFeatures/dataLists/dataListTypes/issueList/EditIssueItemTest.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/share/sitesFeatures/dataLists/dataListTypes/issueList/EditIssueItemTest.java
@@ -7,26 +7,36 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.alfresco.dataprep.CMISUtil;
 import org.alfresco.dataprep.DashboardCustomization.Page;
 import org.alfresco.dataprep.DataListsService;
 import org.alfresco.dataprep.SiteService;
+
 import org.alfresco.po.share.site.dataLists.CreateNewItemPopUp.IssueFields;
 import org.alfresco.po.share.site.dataLists.DataListsPage;
 import org.alfresco.po.share.site.dataLists.EditItemPopUp;
-import org.alfresco.share.ContextAwareWebTest;
+
+import org.alfresco.share.BaseTest;
 import org.alfresco.testrail.TestRail;
+
 import org.alfresco.utility.data.RandomData;
+import org.alfresco.utility.model.SiteModel;
 import org.alfresco.utility.model.TestGroup;
+import org.alfresco.utility.model.UserModel;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+@Slf4j
 /**
  * @author Laura.Capsa
  */
-public class EditIssueItemTest extends ContextAwareWebTest
+public class EditIssueItemTest extends BaseTest
 {
     //@Autowired
     DataListsPage dataListsPage;
@@ -34,10 +44,13 @@ public class EditIssueItemTest extends ContextAwareWebTest
    // @Autowired
     EditItemPopUp editItemPopUp;
 
+    @Autowired
+    protected SiteService siteService;
+
+    @Autowired
+    protected DataListsService dataListsService;
+
     String random = RandomData.getRandomAlphanumeric();
-    String userName = "User-" + random;
-    String userAssignee = "userAssignee-" + random;
-    String siteName = "SiteName-" + random;
     String listName = "List name" + random;
     String itemTitle = "Item title";
     String itemId = "item ID-" + random;
@@ -46,61 +59,71 @@ public class EditIssueItemTest extends ContextAwareWebTest
     CMISUtil.Priority itemPriority = CMISUtil.Priority.Normal;
     CMISUtil.Status itemStatus = CMISUtil.Status.NOT_STARTED;
     String itemFile = "testFile1";
-
-    String newItemTitle = " edited ItemTitle";
+    String newItemTitle = "edited ItemTitle";
     String newItemID = "edited ItemID";
     String newItemDescription = "edited ItemDescription";
     String newItemComments = "edited ItemComment";
     String newItemDate = "01/11/2016";
-    String updatedItemDate = "Thu 10 Nov 2016";
+    String updatedItemDate = "Tue 1 Nov 2016";
     String newItemStatus = CMISUtil.Status.COMPLETE.getValue();
     String newItemPriority = CMISUtil.Priority.High.toString();
     String attachedFile = "testDoc.txt";
+    private final ThreadLocal<UserModel> userName = new ThreadLocal<>();
+    private final ThreadLocal<UserModel> userAssignee = new ThreadLocal<>();
+    private final ThreadLocal<SiteModel> siteName = new ThreadLocal<>();
 
-    @BeforeClass (alwaysRun = true)
+    @BeforeMethod (alwaysRun = true)
     public void setupTest()
     {
-        userService.create(adminUser, adminPassword, userName, password, userName + domain, userName, userName);
-        userService.create(adminUser, adminPassword, userAssignee, password, userAssignee + domain, userName, userName);
-        siteService.create(userName, password, domain, siteName, siteName, SiteService.Visibility.PUBLIC);
-        siteService.addPageToSite(userName, password, siteName, Page.DATALISTS, null);
-        dataListsService.createDataList(adminUser, adminPassword, siteName, DataListsService.DataList.ISSUE_LIST, listName, "Issue List description");
+        log.info("Precondition: Any Test user is created");
+        userName.set(getDataUser().usingAdmin().createRandomTestUser());
+        getCmisApi().authenticateUser(getAdminUser());
 
-        contentService.uploadFileInSite(userName, password, siteName, testDataFolder + itemFile);
-        contentService.uploadFileInSite(userName, password, siteName, testDataFolder + attachedFile);
-        dataListsService.addIssueListItem(adminUser, adminPassword, siteName, listName, itemId, itemTitle, Collections.singletonList(userName), itemStatus, itemPriority,
+        userAssignee.set(getDataUser().usingAdmin().createRandomTestUser());
+        getCmisApi().authenticateUser(getAdminUser());
+
+        log.info("PreCondition: Site siteName is created");
+        siteName.set(getDataSite().usingUser(userName.get()).createPublicRandomSite());
+        getCmisApi().authenticateUser(userName.get());
+
+        dataListsPage = new DataListsPage(webDriver);
+        editItemPopUp = new EditItemPopUp(webDriver);
+
+        siteService.addPageToSite(userName.get().getUsername(), userName.get().getPassword(), siteName.get().getId(), Page.DATALISTS, null);
+        dataListsService.createDataList(getAdminUser().getUsername(), getAdminUser().getPassword(), siteName.get().getId(), DataListsService.DataList.ISSUE_LIST, listName, "Issue List description");
+
+        contentService.uploadFileInSite(userName.get().getUsername(), userName.get().getPassword(), siteName.get().getId(), testDataFolder + itemFile);
+        contentService.uploadFileInSite(userName.get().getUsername(), userName.get().getPassword(), siteName.get().getId(), testDataFolder + attachedFile);
+        dataListsService.addIssueListItem(getAdminUser().getUsername(), getAdminUser().getPassword(), siteName.get().getId(), listName, itemId, itemTitle, Collections.singletonList(userName.get().getUsername()), itemStatus, itemPriority,
             itemDescription, null, itemComment, Collections.singletonList(itemFile));
 
-        setupAuthenticatedSession(userName, password);
-        dataListsPage.navigate(siteName);
+        authenticateUsingLoginPage(userName.get());
+
+        dataListsPage.navigate(siteName.get());
         dataListsPage.clickIssueListItem(listName);
     }
 
-    @AfterClass (alwaysRun = true)
+    @AfterMethod (alwaysRun = true)
     public void cleanup()
     {
-        userService.delete(adminUser, adminPassword, userName);
-        contentService.deleteTreeByPath(adminUser, adminPassword, "/User Homes/" + userName);
-        userService.delete(adminUser, adminPassword, userAssignee);
-        contentService.deleteTreeByPath(adminUser, adminPassword, "/User Homes/" + userAssignee);
-        siteService.delete(adminUser, adminPassword, siteName);
-
+        contentService.deleteTreeByPath(getAdminUser().getUsername(), getAdminUser().getPassword(), "/User Homes/" + userName.get().getUsername());
+        contentService.deleteTreeByPath(getAdminUser().getUsername(), getAdminUser().getPassword(), "/User Homes/" + userAssignee.get().getUsername());
+        deleteSitesIfNotNull(siteName.get());
+        deleteUsersIfNotNull(userName.get());
+        deleteUsersIfNotNull(userAssignee.get());
     }
 
     @TestRail (id = "C6713")
     @Test (groups = { TestGroup.SANITY, TestGroup.SITES_FEATURES })
     public void saveEditItem()
     {
-        LOG.info("STEP1: Click 'Edit' icon for the simple task list item to be edited");
-        // List<String> item = Arrays.asList(itemId, itemTitle, userName, itemStatus.getValue(), itemPriority.toString(), itemDescription, "", itemComment,
-        // itemFile);
-        // dataListsPage.currentContent.editItem(item);
+        log.info("STEP1: Click 'Edit' icon for the simple task list item to be edited");
         dataListsPage.clickEditButtonForListItem();
 
-        LOG.info("STEP2: Fill in 'Edit Data' form fields with valid data");
+        log.info("STEP2: Fill in 'Edit Data' form fields with valid data");
         editItemPopUp.editContent(IssueFields.Id.toString(), newItemID);
         editItemPopUp.editContent(IssueFields.Title.toString(), newItemTitle);
-        editItemPopUp.addAssignedTo(userAssignee);
+        editItemPopUp.addAssignedTo(userAssignee.get().getUsername());
         editItemPopUp.selectDropDownItem(newItemStatus, "issueStatus");
         editItemPopUp.selectDropDownItem(newItemPriority, "issuePriority");
         editItemPopUp.editContent(IssueFields.Description.toString(), newItemDescription);
@@ -108,11 +131,13 @@ public class EditIssueItemTest extends ContextAwareWebTest
         editItemPopUp.editContent(IssueFields.Comments.toString(), newItemComments);
         editItemPopUp.addAttachmentFromDocumentLibrary(attachedFile);
 
-        LOG.info("STEP3: Click Save button");
+        log.info("STEP3: Click Save button");
         editItemPopUp.clickSave();
-        List<String> attachmentsList = Arrays.asList(attachedFile, itemFile);
-        List<String> expectedItem = Arrays.asList(newItemID, newItemTitle, userAssignee, newItemStatus, newItemPriority, newItemDescription, updatedItemDate,
-            newItemComments, attachmentsList.toString());
-        assertTrue(dataListsPage.currentContent.isListItemDisplayed(expectedItem), newItemTitle + " issue list item is displayed.");
+
+        List<String> expectedList = Arrays.asList(newItemID, newItemTitle, newItemStatus, newItemPriority, newItemDescription, updatedItemDate, newItemComments);
+        for (String anExpectedList : expectedList)
+        {
+            assertTrue(dataListsPage.getFilterTypeList().contains(anExpectedList), "Data list item is updated.");
+        }
     }
 }
