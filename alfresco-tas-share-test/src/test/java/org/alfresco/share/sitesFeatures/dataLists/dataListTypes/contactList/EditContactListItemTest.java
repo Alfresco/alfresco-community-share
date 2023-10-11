d838d0a41dbc694e879ae94a7e99cab8580a2160
ggnanadhas <godwin.gnanadhas@globallogic.com>
2023-10-11 10:24:50 +0530

    Test package "dataListTypes" refactored

--- alfresco-tas-share-test/src/test/java/org/alfresco/share/sitesFeatures/dataLists/dataListTypes/contactList/EditContactListItemTest.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/share/sitesFeatures/dataLists/dataListTypes/contactList/EditContactListItemTest.java
@@ -6,25 +6,35 @@ import static org.testng.Assert.assertTrue;
 import java.util.Arrays;
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.alfresco.dataprep.DashboardCustomization.Page;
 import org.alfresco.dataprep.DataListsService;
 import org.alfresco.dataprep.SiteService;
+
 import org.alfresco.po.share.site.dataLists.CreateNewItemPopUp.ContactListFields;
 import org.alfresco.po.share.site.dataLists.DataListsPage;
 import org.alfresco.po.share.site.dataLists.EditItemPopUp;
-import org.alfresco.share.ContextAwareWebTest;
+
+import org.alfresco.share.BaseTest;
+
 import org.alfresco.testrail.TestRail;
-import org.alfresco.utility.data.RandomData;
+
+import org.alfresco.utility.model.SiteModel;
 import org.alfresco.utility.model.TestGroup;
+import org.alfresco.utility.model.UserModel;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
+@Slf4j
 /**
  * @author Laura.Capsa
  */
-public class EditContactListItemTest extends ContextAwareWebTest
+public class EditContactListItemTest extends BaseTest
 {
     //@Autowired
     DataListsPage dataListsPage;
@@ -32,8 +42,14 @@ public class EditContactListItemTest extends ContextAwareWebTest
     //@Autowired
     EditItemPopUp editItemPopUp;
 
-    String userName = String.format("User-%s", RandomData.getRandomAlphanumeric());
-    String siteName = String.format("SiteName%s", RandomData.getRandomAlphanumeric());
+    @Autowired
+    protected SiteService siteService;
+
+    @Autowired
+    protected DataListsService dataListsService;
+
+    private final ThreadLocal<UserModel> userName = new ThreadLocal<>();
+    private final ThreadLocal<SiteModel> siteName = new ThreadLocal<>();
     String contactListName = "contactList";
     String editedFirstName = "editedFirstName";
     String editedLastName = "editedLastName";
@@ -44,67 +60,78 @@ public class EditContactListItemTest extends ContextAwareWebTest
     String editedPhoneMobile = "0123456780";
     String editedNotes = "editedNotes";
 
-    @BeforeClass (alwaysRun = true)
+    @BeforeMethod(alwaysRun = true)
     public void setupTest()
     {
-        userService.create(adminUser, adminPassword, userName, password, userName + domain, userName, userName);
-        siteService.create(userName, password, domain, siteName, siteName, SiteService.Visibility.PUBLIC);
-        siteService.addPageToSite(userName, password, siteName, Page.DATALISTS, null);
-        dataListsService.createDataList(adminUser, adminPassword, siteName, DataListsService.DataList.CONTACT_LIST, contactListName, "Contact list description");
-        dataListsService.addContactListItem(adminUser, adminPassword, siteName, contactListName, "firstName", "lastName", "test@test.com", "companyName", "jobTitle",
+        log.info("Precondition: Any Test user is created");
+        userName.set(getDataUser().usingAdmin().createRandomTestUser());
+        getCmisApi().authenticateUser(getAdminUser());
+
+        log.info("PreCondition: Site siteName is created");
+        siteName.set(getDataSite().usingUser(userName.get()).createPublicRandomSite());
+        getCmisApi().authenticateUser(userName.get());
+
+        dataListsPage = new DataListsPage(webDriver);
+        editItemPopUp = new EditItemPopUp(webDriver);
+
+        siteService.addPageToSite(userName.get().getUsername(), userName.get().getPassword(), siteName.get().getId(), Page.DATALISTS, null);
+        dataListsService.createDataList(userName.get().getUsername(), userName.get().getPassword(), siteName.get().getId(), DataListsService.DataList.CONTACT_LIST, contactListName, "Contact list description");
+        dataListsService.addContactListItem(userName.get().getUsername(), userName.get().getPassword(), siteName.get().getId(), contactListName, "firstName", "lastName", "test@test.com", "companyName", "jobTitle",
             "123456", "+41256422", "testNotes");
 
-        setupAuthenticatedSession(userName, password);
-        dataListsPage.navigate(siteName);
+        authenticateUsingLoginPage(userName.get());
+
+        dataListsPage.navigate(siteName.get());
         dataListsPage.clickContactListItem(contactListName);
     }
 
-    @AfterClass (alwaysRun = true)
+    @AfterMethod(alwaysRun = true)
     public void cleanup()
     {
-        userService.delete(adminUser, adminPassword, userName);
-        contentService.deleteTreeByPath(adminUser, adminPassword, "/User Homes/" + userName);
-        siteService.delete(adminUser, adminPassword, siteName);
+        contentService.deleteTreeByPath(getAdminUser().getUsername(), getAdminUser().getPassword(), "/User Homes/" + userName.get().getUsername());
+        deleteSitesIfNotNull(siteName.get());
+        deleteUsersIfNotNull(userName.get());
 
     }
 
     @TestRail (id = "C6549")
-    @Test (groups = { TestGroup.SANITY, TestGroup.SITES_FEATURES, "tobefixed" })
-    public void verifyPossibilityToEditItem()
-    {
-        LOG.info("STEP1: Click the 'Edit' icon for the contact list item to be edited");
-        dataListsPage.currentContent
-            .editItem(Arrays.asList("firstName", "lastName", "test@test.com", "companyName", "jobTitle", "123456", "+41256422", "testNotes"));
+    @Test(groups = { TestGroup.SANITY, TestGroup.SITES_FEATURES, "tobefixed" })
+    public void verifyPossibilityToEditItem() throws InterruptedException {
+        log.info("STEP1: Click the 'Edit' icon for the contact list item to be edited");
+        dataListsPage.clickEditButtonForListItem();
 
-        LOG.info("STEP2: Edit the First Name");
+        log.info("STEP2: Edit the First Name");
         editItemPopUp.editContent(ContactListFields.FirstName.toString(), editedFirstName);
 
-        LOG.info("STEP3: Edit Last Name field ");
+        log.info("STEP3: Edit Last Name field ");
         editItemPopUp.editContent(ContactListFields.LastName.toString(), editedLastName);
 
-        LOG.info("STEP4: Edit Email field");
+        log.info("STEP4: Edit Email field");
         editItemPopUp.editContent(ContactListFields.Email.toString(), editedEmail);
 
-        LOG.info("STEP5: Edit Company field");
+        log.info("STEP5: Edit Company field");
         editItemPopUp.editContent(ContactListFields.Company.toString(), editedCompany);
 
-        LOG.info("STEP6: Edit Job title field");
+        log.info("STEP6: Edit Job title field");
         editItemPopUp.editContent(ContactListFields.JobTitle.toString(), editedJobTitle);
 
-        LOG.info("STEP: Edit Phone (Office) field");
+        log.info("STEP: Edit Phone (Office) field");
         editItemPopUp.editContent(ContactListFields.PhoneOffice.toString(), editedPhoneOffice);
 
-        LOG.info("STEP: Edit Phone (Mobile) field");
+        log.info("STEP: Edit Phone (Mobile) field");
         editItemPopUp.editContent(ContactListFields.PhoneMobile.toString(), editedPhoneMobile);
 
-        LOG.info("STEP: Edit Notes text area");
+        log.info("STEP: Edit Notes text area");
         editItemPopUp.editContent(ContactListFields.Notes.toString(), editedNotes);
 
-        LOG.info("STEP10: Click Save button");
+        log.info("STEP10: Click Save button");
         editItemPopUp.clickSave();
-        assertEquals(dataListsPage.currentContent.messageDisplayed(), "Data Item updated successfully", "The pop-up message isn't as expected.");
+        assertEquals(dataListsPage.messageDisplayed(), "Data Item updated successfully", "The pop-up message isn't as expected.");
         List<String> expectedList = Arrays.asList(editedFirstName, editedLastName, editedEmail, editedCompany, editedJobTitle, editedPhoneOffice,
             editedPhoneMobile, editedNotes);
-        assertTrue(dataListsPage.currentContent.isListItemDisplayed(expectedList), "Data list item is updated.");
+        for (String anExpectedList : expectedList)
+        {
+            assertTrue(dataListsPage.getFilterTypeList().contains(anExpectedList), "Data list item is updated.");
+        }
     }
 }
