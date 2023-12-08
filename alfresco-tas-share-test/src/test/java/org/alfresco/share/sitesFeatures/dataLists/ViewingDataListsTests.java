b61db15409fd817163225203389c8d11d27fdfc1
ggnanadhas <godwin.gnanadhas@globallogic.com>
2023-12-08 08:47:01 +0530

    Test Class "ViewingDataListsTests" Refactored

--- alfresco-tas-share-test/src/test/java/org/alfresco/share/sitesFeatures/dataLists/ViewingDataListsTests.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/share/sitesFeatures/dataLists/ViewingDataListsTests.java
@@ -2,79 +2,87 @@ package org.alfresco.share.sitesFeatures.dataLists;
 
 import static org.testng.Assert.assertTrue;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.alfresco.dataprep.DashboardCustomization.Page;
+import org.alfresco.dataprep.DataListsService;
 import org.alfresco.dataprep.DataListsService.DataList;
 import org.alfresco.dataprep.SiteService;
+
 import org.alfresco.po.share.site.dataLists.ContactListSelectedContent.ContactListColumns;
 import org.alfresco.po.share.site.dataLists.DataListsPage;
-import org.alfresco.share.ContextAwareWebTest;
+
+import org.alfresco.share.BaseTest;
 import org.alfresco.testrail.TestRail;
-import org.alfresco.utility.data.RandomData;
+
+import org.alfresco.utility.model.SiteModel;
 import org.alfresco.utility.model.TestGroup;
+import org.alfresco.utility.model.UserModel;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-public class ViewingDataListsTests extends ContextAwareWebTest
+@Slf4j
+public class ViewingDataListsTests extends BaseTest
 {
     //@Autowired
     DataListsPage dataListsPage;
-
-    private String userName;
-    private String siteName;
-
-    @BeforeClass (alwaysRun = true)
-    public void createUser()
-    {
-        userName = String.format("User%s", RandomData.getRandomAlphanumeric());
-        userService.create(adminUser, adminPassword, userName, password, userName + domain, userName, userName);
-        setupAuthenticatedSession(userName, password);
-    }
+    @Autowired
+    SiteService siteService;
+    @Autowired
+    DataListsService dataListsService;
+    private final ThreadLocal<UserModel> userName = new ThreadLocal<>();
+    private final ThreadLocal<SiteModel> siteName = new ThreadLocal<>();
+    String domain = "@test.com";
 
     @BeforeMethod (alwaysRun = true)
     public void precondition()
     {
-        siteName = String.format("siteName%s", RandomData.getRandomAlphanumeric());
-        siteService.create(userName, password, domain, siteName, siteName, SiteService.Visibility.PUBLIC);
-        siteService.addPageToSite(userName, password, siteName, Page.DATALISTS, null);
-    }
+        log.info("Precondition: Any Test user is created");
+        userName.set(getDataUser().usingAdmin().createRandomTestUser());
+        getCmisApi().authenticateUser(getAdminUser());
 
-    @AfterClass (alwaysRun = true)
-    public void cleanup()
-    {
-        userService.delete(adminUser, adminPassword, userName);
-        contentService.deleteTreeByPath(adminUser, adminPassword, "/User Homes/" + userName);
+        log.info("PreCondition: Site siteName is created");
+        siteName.set(getDataSite().usingUser(userName.get()).createPublicRandomSite());
+        getCmisApi().authenticateUser(userName.get());
+
+        siteService.addPageToSite(userName.get().getUsername(), userName.get().getPassword(), siteName.get().getId(), Page.DATALISTS, null);
+
+        dataListsPage = new DataListsPage(webDriver);
+
+
+        authenticateUsingLoginPage(userName.get());
     }
 
     @AfterMethod (alwaysRun = true)
     public void cleanupMethod()
     {
-        siteService.delete(adminUser, adminPassword, siteName);
-
+        contentService.deleteTreeByPath(getAdminUser().getUsername(), getAdminUser().getPassword(), "/User Homes/" + userName.get().getUsername());
+        deleteSitesIfNotNull(siteName.get());
+        deleteUsersIfNotNull(userName.get());
     }
 
     @TestRail (id = "C5853")
     @Test (groups = { TestGroup.SANITY, TestGroup.SITES_FEATURES })
     public void filterAreVisibleOnlyIfAListIsSelected()
     {
-        LOG.info("Preconditions: Create a new List");
+        log.info("Preconditions: Create a new List");
         String listName = "list" + System.currentTimeMillis();
-        dataListsService.createDataList(adminUser, adminPassword, siteName, DataList.CONTACT_LIST, listName, "contact link description");
+        dataListsService.createDataList(getAdminUser().getUsername(), getAdminUser().getPassword(), siteName.get().getId(), DataList.CONTACT_LIST, listName, "contact link description");
 
-        dataListsPage.navigate(siteName);
+        dataListsPage.navigate(siteName.get().getId());
 
-        LOG.info("Step 1: Check the Items filter");
-        Assert.assertFalse(dataListsPage.currentContent.allFilterOptionsAreDisplayed(), "The filter options are displayed.");
+        log.info("Step 1: Check the Items filter");
+        Assert.assertFalse(dataListsPage.allFilterOptionsAreDisplayed(), "The filter options are displayed.");
 
-        LOG.info("Step 2: Click the created list displayed under Lists view.");
+        log.info("Step 2: Click the created list displayed under Lists view.");
         dataListsPage.clickContactListItem(listName);
 
-        LOG.info("Step 3: Check the available filtering options.");
-        assertTrue(dataListsPage.currentContent.allFilterOptionsAreDisplayed(), "Not all filters are displayed.");
+        log.info("Step 3: Check the available filtering options.");
+        assertTrue(dataListsPage.allFilterOptionsAreDisplayed(), "Not all filters are displayed.");
     }
 
     @TestRail (id = "C5854")
@@ -117,81 +125,81 @@ public class ViewingDataListsTests extends ContextAwareWebTest
     @Test (groups = { TestGroup.SANITY, TestGroup.SITES_FEATURES })
     public void multiplePagesViewedInDataListsExplorerPanel()
     {
-        LOG.info("Preconditions: Create a new List and add new items to it");
+        log.info("Preconditions: Create a new List and add new items to it");
         String listName = "list" + System.currentTimeMillis();
-        dataListsService.createDataList(adminUser, adminPassword, siteName, DataList.CONTACT_LIST, listName, "contact link description");
+        dataListsService.createDataList(getAdminUser().getUsername(), getAdminUser().getPassword(), siteName.get().getId(), DataList.CONTACT_LIST, listName, "contact link description");
 
         for (int i = 0; i < 51; i++)
         {
-            dataListsService.addContactListItem(userName, password, siteName, listName, userName + i, userName + i, userName + i + domain, "companyName", "jobTitle", "phoneOffice", "phoneMobile", "notes");
+            dataListsService.addContactListItem(userName.get().getUsername(), userName.get().getPassword(), siteName.get().getId(), listName, userName.get().getFirstName() + i, userName.get().getLastName() + i, userName.get().getUsername() + i + domain, "companyName", "jobTitle", "phoneOffice", "phoneMobile", "notes");
         }
 
-        dataListsPage.navigate(siteName);
+        dataListsPage.navigate(siteName.get().getId());
 
-        LOG.info("Step 1: Click the created list displayed under Lists view.");
+        log.info("Step 1: Click the created list displayed under Lists view.");
         dataListsPage.clickContactListItem(listName);
-        assertTrue(dataListsPage.currentContent.areNavigationLinksDisplayed(), "The navigation links are not displayed.");
-        Assert.assertEquals(dataListsPage.currentContent.getCurrentPageNumber(), "1", "The current page is not the expected one.");
+        assertTrue(dataListsPage.areNavigationLinksDisplayed(), "The navigation links are not displayed.");
+        Assert.assertEquals(dataListsPage.getCurrentPageNumber(), "1", "The current page is not the expected one.");
 
-        LOG.info("Step 2: Click on the 'next' navigation item.");
-        dataListsPage.currentContent.clickNextNavigationItem();
-        Assert.assertEquals(dataListsPage.currentContent.getCurrentPageNumber(), "2", "The current page is not the expected one.");
+        log.info("Step 2: Click on the 'next' navigation item.");
+        dataListsPage.clickNextNavigationItem();
+        Assert.assertEquals(dataListsPage.getCurrentPageNumber(), "2", "The current page is not the expected one.");
 
-        LOG.info("Step 3: Click on the 'previous' navigation item.");
-        dataListsPage.currentContent.clickPreviousNavigationItem();
-        Assert.assertEquals(dataListsPage.currentContent.getCurrentPageNumber(), "1", "The current page is not the expected one.");
+        log.info("Step 3: Click on the 'previous' navigation item.");
+        dataListsPage.clickPreviousNavigationItem();
+        Assert.assertEquals(dataListsPage.getCurrentPageNumber(), "1", "The current page is not the expected one.");
 
-        LOG.info("Step 3: Click on page number '2'.");
-        dataListsPage.currentContent.clickOnSpecificPage("2");
-        Assert.assertEquals(dataListsPage.currentContent.getCurrentPageNumber(), "2", "The current page is not the expected one.");
+        log.info("Step 3: Click on page number '2'.");
+        dataListsPage.clickOnSpecificPage("2");
+        Assert.assertEquals(dataListsPage.getCurrentPageNumber(), "2", "The current page is not the expected one.");
     }
 
     @TestRail (id = "C5859")
-    @Test (groups = { TestGroup.SANITY, TestGroup.SITES_FEATURES, "tobefixed" })
+    @Test (groups = { TestGroup.SANITY, TestGroup.SITES_FEATURES })
     public void dataListItemsSortedByColumn()
     {
-        LOG.info("Preconditions: Create a new List and add two items to it, descendent ordered by Company Name");
+        log.info("Preconditions: Create a new List and add two items to it, descendent ordered by Company Name");
         String listName = "list" + System.currentTimeMillis();
-        dataListsService.createDataList(adminUser, adminPassword, siteName, DataList.CONTACT_LIST, listName, "contact link description");
+        dataListsService.createDataList(getAdminUser().getUsername(), getAdminUser().getPassword(), siteName.get().getId(), DataList.CONTACT_LIST, listName, "contact link description");
 
         for (int i = 0; i < 2; i++)
         {
-            dataListsService.addContactListItem(userName, password, siteName, listName, userName + i, userName + i, userName + i + domain, i + "comapanyName", "jobTitle", "phoneOffice", "phoneMobile", "notes");
+            dataListsService.addContactListItem(userName.get().getUsername(), userName.get().getPassword(), siteName.get().getId(), listName, userName.get().getFirstName() + i, userName.get().getLastName() + i, userName.get().getUsername() + i + domain, i + "comapanyName", "jobTitle", "phoneOffice", "phoneMobile", "notes");
         }
 
-        dataListsPage.navigate(siteName);
+        dataListsPage.navigate(siteName.get().getId());
 
-        LOG.info("Step 1: Click the created list displayed under Lists view.");
+        log.info("Step 1: Click the created list displayed under Lists view.");
         dataListsPage.clickContactListItem(listName);
 
-        LOG.info("Step 2: In the table click 'Company' column headings and check the items are sorted by Company Name.");
-        assertTrue(dataListsPage.currentContent.areItemsSortedByColumnAfterClickingTheColumn(ContactListColumns.Company.toString()), "The data list items are sorted by Company");
+        log.info("Step 2: In the table click 'Company' column headings and check the items are sorted by Company Name.");
+        assertTrue(dataListsPage.areItemsSortedByColumnAfterClickingTheColumn(ContactListColumns.Company.toString()), "The data list items are sorted by Company");
     }
 
     @TestRail (id = "C5860")
     @Test (groups = { TestGroup.SANITY, TestGroup.SITES_FEATURES })
     public void aSinglePageOfListItemsViewed()
     {
-        LOG.info("Preconditions: Create a new List and add two items to it, descendent ordered by Company Name");
+        log.info("Preconditions: Create a new List and add two items to it, descendent ordered by Company Name");
         String listName = "list" + System.currentTimeMillis();
-        dataListsService.createDataList(adminUser, adminPassword, siteName, DataList.CONTACT_LIST, listName, "contact link description");
+        dataListsService.createDataList(getAdminUser().getUsername(), getAdminUser().getPassword(), siteName.get().getId(), DataList.CONTACT_LIST, listName, "contact link description");
 
         for (int i = 0; i < 2; i++)
         {
-            dataListsService.addContactListItem(userName, password, siteName, listName, userName + i, userName + i, userName + i + domain, i + "comapanyName", "jobTitle", "phoneOffice", "phoneMobile", "notes");
+            dataListsService.addContactListItem(userName.get().getUsername(), userName.get().getPassword(), siteName.get().getId(), listName, userName.get().getFirstName() + i, userName.get().getLastName() + i, userName.get().getUsername() + i + domain, i + "comapanyName", "jobTitle", "phoneOffice", "phoneMobile", "notes");
         }
 
-        dataListsPage.navigate(siteName);
+        dataListsPage.navigate(siteName.get().getId());
 
-        LOG.info("Step 1: Click the created list displayed under Lists view.");
+        log.info("Step 1: Click the created list displayed under Lists view.");
         dataListsPage.clickContactListItem(listName);
 
-        LOG.info("Step 2: Click the 'next' navigation item.");
-        dataListsPage.currentContent.clickNextNavigationItem();
-        Assert.assertEquals(dataListsPage.currentContent.getCurrentPageNumber(), "1", "The current page is not the expected one.");
+        log.info("Step 2: Click the 'next' navigation item.");
+        dataListsPage.clickNextNavigationItem();
+        Assert.assertEquals(dataListsPage.getCurrentPageNumber(), "1", "The current page is not the expected one.");
 
-        LOG.info("Step 3: Click on the 'previous' navigation item.");
-        dataListsPage.currentContent.clickPreviousNavigationItem();
-        Assert.assertEquals(dataListsPage.currentContent.getCurrentPageNumber(), "1", "The current page is not the expected one.");
+        log.info("Step 3: Click on the 'previous' navigation item.");
+        dataListsPage.clickPreviousNavigationItem();
+        Assert.assertEquals(dataListsPage.getCurrentPageNumber(), "1", "The current page is not the expected one.");
     }
 }
