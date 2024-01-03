76fe2deb54d8a3920042a0da4381680331f1271b
ggnanadhas <godwin.gnanadhas@globallogic.com>
2024-01-03 14:23:47 +0530

    Test Class "ViewTopicTests" Refactored

--- alfresco-tas-share-test/src/test/java/org/alfresco/share/sitesFeatures/discussions/ViewTopicTests.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/share/sitesFeatures/discussions/ViewTopicTests.java
@@ -6,26 +6,40 @@ import static org.testng.Assert.assertTrue;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+
 import java.util.Collections;
 import java.util.Date;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.alfresco.dataprep.DashboardCustomization.Page;
+import org.alfresco.dataprep.SitePagesService;
 import org.alfresco.dataprep.SiteService;
+import org.alfresco.dataprep.UserService;
+
 import org.alfresco.po.share.site.discussion.TopicListPage;
 import org.alfresco.po.share.site.discussion.TopicViewPage;
-import org.alfresco.share.ContextAwareWebTest;
+
+import org.alfresco.share.BaseTest;
+
 import org.alfresco.testrail.TestRail;
-import org.alfresco.utility.data.RandomData;
+
+import org.alfresco.utility.model.SiteModel;
 import org.alfresco.utility.model.TestGroup;
+import org.alfresco.utility.model.UserModel;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+@Slf4j
 
 /**
  * Created by Claudia Agache on 8/10/2016.
  */
-public class ViewTopicTests extends ContextAwareWebTest
+public class ViewTopicTests extends BaseTest
 {
     //@Autowired
     TopicListPage topicListPage;
@@ -33,37 +47,58 @@ public class ViewTopicTests extends ContextAwareWebTest
     //@Autowired
     TopicViewPage topicViewPage;
 
+    @Autowired
+    protected UserService userService;
+
+    @Autowired
+    protected SiteService siteService;
+
+    @Autowired
+    protected SitePagesService sitePagesService;
+
     DateFormat df = new SimpleDateFormat("EE d MMM yyyy");
     String today;
-    private String user1 = String.format("User1%s", RandomData.getRandomAlphanumeric());
-    private String user2 = String.format("User2%s", RandomData.getRandomAlphanumeric());
-    private String siteName = String.format("Site1%s", RandomData.getRandomAlphanumeric());
+    private final ThreadLocal<UserModel> user1 = new ThreadLocal<>();
+    private final ThreadLocal<UserModel> user2 = new ThreadLocal<>();
+    private final ThreadLocal<SiteModel> siteName = new ThreadLocal<>();
     private String topicTitle = "Topic1";
     private String topicContent = "Some content";
     private String topicTag = "tag1";
     private String topicReply = "Some reply from user2.";
 
-    @BeforeClass (alwaysRun = true)
+    @BeforeMethod (alwaysRun = true)
     public void setupTest()
     {
-        userService.create(adminUser, adminPassword, user1, password, user1 + domain, user1, "lName1");
-        userService.create(adminUser, adminPassword, user2, password, user2 + domain, user2, "lName2");
-        siteService.create(user1, password, domain, siteName, "description", SiteService.Visibility.PUBLIC);
-        userService.createSiteMember(user1, password, user2, siteName, "SiteManager");
-        siteService.addPageToSite(user1, password, siteName, Page.DISCUSSIONS, null);
-        sitePagesService.createDiscussion(user1, password, siteName, topicTitle, topicContent, Collections.singletonList(topicTag));
-        sitePagesService.replyToDiscussion(user2, password, siteName, topicTitle, topicReply);
-        setupAuthenticatedSession(user1, password);
+        log.info("Precondition: Any Test user is created");
+        user1.set(getDataUser().usingAdmin().createRandomTestUser());
+        getCmisApi().authenticateUser(getAdminUser());
+
+        user2.set(getDataUser().usingAdmin().createRandomTestUser());
+        getCmisApi().authenticateUser(getAdminUser());
+
+        log.info("PreCondition: Site siteName is created");
+        siteName.set(getDataSite().usingUser(user1.get()).createPublicRandomSite());
+        getCmisApi().authenticateUser(user1.get());
+
+        userService.createSiteMember(user1.get().getUsername(), user1.get().getPassword(), user2.get().getUsername(), siteName.get().getId(), "SiteManager");
+        siteService.addPageToSite(user1.get().getUsername(), user1.get().getPassword(), siteName.get().getId(), Page.DISCUSSIONS, null);
+        sitePagesService.createDiscussion(user1.get().getUsername(), user1.get().getPassword(), siteName.get().getId(), topicTitle, topicContent, Collections.singletonList(topicTag));
+        sitePagesService.replyToDiscussion(user2.get().getUsername(), user2.get().getPassword(), siteName.get().getId(), topicTitle, topicReply);
+
+        topicListPage = new TopicListPage(webDriver);
+        topicViewPage = new TopicViewPage(webDriver);
+
+        authenticateUsingLoginPage(user1.get());
     }
 
-    @AfterClass (alwaysRun = true)
+    @AfterMethod (alwaysRun = true)
     public void cleanup()
     {
-        userService.delete(adminUser, adminPassword, user1);
-        contentService.deleteTreeByPath(adminUser, adminPassword, "/User Homes/" + user1);
-        userService.delete(adminUser, adminPassword, user2);
-        contentService.deleteTreeByPath(adminUser, adminPassword, "/User Homes/" + user2);
-        siteService.delete(adminUser, adminPassword, siteName);
+        contentService.deleteTreeByPath(getAdminUser().getUsername(), getAdminUser().getPassword(), "/User Homes/" + user1.get().getUsername());
+        contentService.deleteTreeByPath(getAdminUser().getUsername(), getAdminUser().getPassword(), "/User Homes/" + user2.get().getUsername());
+        deleteSitesIfNotNull(siteName.get());
+        deleteUsersIfNotNull(user1.get());
+        deleteUsersIfNotNull(user2.get());
     }
 
     @TestRail (id = "6211")
@@ -72,28 +107,28 @@ public class ViewTopicTests extends ContextAwareWebTest
     {
         today = df.format(new Date());
         //precondition
-        topicListPage.navigate(siteName);
+        topicListPage.navigate(siteName.get());
 
-        LOG.info("STEP 1 - Click on 'View' topic link.");
+        log.info("STEP 1 - Click on 'View' topic link.");
         topicListPage.viewTopic(topicTitle);
 
-        LOG.info("Expected Result: Information about that topic is displayed.");
+        log.info("Expected Result: Information about that topic is displayed.");
         assertEquals(topicViewPage.getTopicTitle(), topicTitle, "Title is displayed.");
         assertTrue(topicViewPage.getTopicPublished().startsWith("Created on: " + today),
             "Topic was created today. Actual: [" + topicViewPage.getTopicPublished() + "]. Expected: [" + today + "]");
-        assertTrue(topicViewPage.getTopicPublished().contains("Author: " + user1 + " lName1"), "The user who created the topic is User1.");
-        assertTrue(topicViewPage.getTopicPublished().contains("Last reply by: " + user2 + " lName2"), "The user who last replied the topic is User2.");
+        assertTrue(topicViewPage.getTopicPublished().contains("Author: " + user1.get().getFirstName() + " " + user1.get().getLastName()), "The user who created the topic is User1.");
+        assertTrue(topicViewPage.getTopicPublished().contains("Last reply by: " + user2.get().getFirstName() + " " + user2.get().getLastName()), "The user who last replied the topic is User2.");
         assertTrue(topicViewPage.getTopicPublished().contains("Last reply on: " + today), "Last reply was today.");
         assertEquals(topicViewPage.getTopicContent(), topicContent, "Content is displayed.");
         assertEquals(topicViewPage.getTopicReplies(), "(1)", "Topic has 1 reply.");
         assertEquals(topicViewPage.getTopicTags(), topicTag, "Tag is displayed.");
 
-        LOG.info("Expected Result: Information about the reply is displayed.");
-        assertEquals(topicViewPage.getReplyAuthor(topicReply), user2 + " lName2", user2 + " posted that reply.");
+        log.info("Expected Result: Information about the reply is displayed.");
+        assertEquals(topicViewPage.getReplyAuthor(topicReply), user2.get().getFirstName() + " " + user2.get().getLastName(), user2 + " posted that reply.");
         assertTrue(topicViewPage.getReplyDate(topicReply).contains(today), "Reply was posted today.");
         assertEquals(topicViewPage.getReplyNoReplies(topicReply), "(0)", "Reply has 0 replies.");
 
-        LOG.info("STEP 2 - Click 'Discussions Topic List' to return to the main view.");
+        log.info("STEP 2 - Click 'Discussions Topic List' to return to the main view.");
         topicViewPage.clickDiscussionsTopicListLink();
         assertTrue(topicListPage.isTopicDisplayed(topicTitle), "Topic is displayed on the list.");
     }
@@ -104,28 +139,28 @@ public class ViewTopicTests extends ContextAwareWebTest
     {
         today = df.format(new Date());
         //precondition
-        topicListPage.navigate(siteName);
+        topicListPage.navigate(siteName.get());
 
-        LOG.info("STEP 1 - Click on 'Read' link.");
+        log.info("STEP 1 - Click on 'Read' link.");
         topicListPage.readTopic(topicTitle);
 
-        LOG.info("Expected Result: Information about that topic is displayed.");
+        log.info("Expected Result: Information about that topic is displayed.");
         assertEquals(topicViewPage.getTopicTitle(), topicTitle, "Title is displayed.");
         assertTrue(topicViewPage.getTopicPublished().startsWith("Created on: " + today),
             "Topic was created today. Actual: [" + topicViewPage.getTopicPublished() + "]. Expected: [" + today + "]");
-        assertTrue(topicViewPage.getTopicPublished().contains("Author: " + user1 + " lName1"), "The user who created the topic is User1.");
-        assertTrue(topicViewPage.getTopicPublished().contains("Last reply by: " + user2 + " lName2"), "The user who last replied the topic is User2.");
+        assertTrue(topicViewPage.getTopicPublished().contains("Author: " + user1.get().getFirstName() + " " + user1.get().getLastName()), "The user who created the topic is User1.");
+        assertTrue(topicViewPage.getTopicPublished().contains("Last reply by: " + user2.get().getFirstName() + " " + user2.get().getLastName()), "The user who last replied the topic is User2.");
         assertTrue(topicViewPage.getTopicPublished().contains("Last reply on: " + today), "Last reply was today.");
         assertEquals(topicViewPage.getTopicContent(), topicContent, "Content is displayed.");
         assertEquals(topicViewPage.getTopicReplies(), "(1)", "Topic has 1 reply.");
         assertEquals(topicViewPage.getTopicTags(), topicTag, "Tag is displayed.");
 
-        LOG.info("Expected Result: Information about the reply is displayed.");
-        assertEquals(topicViewPage.getReplyAuthor(topicReply), user2 + " lName2", user2 + " posted that reply.");
+        log.info("Expected Result: Information about the reply is displayed.");
+        assertEquals(topicViewPage.getReplyAuthor(topicReply), user2.get().getFirstName() + " " + user2.get().getLastName(), user2 + " posted that reply.");
         assertTrue(topicViewPage.getReplyDate(topicReply).contains(today), "Reply was posted today.");
         assertEquals(topicViewPage.getReplyNoReplies(topicReply), "(0)", "Reply has 0 replies.");
 
-        LOG.info("STEP 2 - Click 'Discussions Topic List' to return to the main view. Choose 'Simple View' to display the page.");
+        log.info("STEP 2 - Click 'Discussions Topic List' to return to the main view. Choose 'Simple View' to display the page.");
         topicViewPage.clickDiscussionsTopicListLink();
         topicListPage.toggleBetweenSimpleAndDetailedView();
         assertFalse(topicListPage.isReadLinkDisplayed(topicTitle), "The Read action is not available if 'Simple View' is selected.");
@@ -137,24 +172,24 @@ public class ViewTopicTests extends ContextAwareWebTest
     {
         today = df.format(new Date());
         //precondition
-        topicListPage.navigate(siteName);
+        topicListPage.navigate(siteName.get());
 
-        LOG.info("STEP 1 - Click on the name of the topic to open it.");
+        log.info("STEP 1 - Click on the name of the topic to open it.");
         topicListPage.clickTopicTitle(topicTitle);
 
-        LOG.info("Expected Result: Information about that topic is displayed.");
+        log.info("Expected Result: Information about that topic is displayed.");
         assertEquals(topicViewPage.getTopicTitle(), topicTitle, "Title is displayed.");
         assertTrue(topicViewPage.getTopicPublished().startsWith("Created on: " + today),
             "Topic was created today. Actual: [" + topicViewPage.getTopicPublished() + "]. Expected: [" + today + "]");
-        assertTrue(topicViewPage.getTopicPublished().contains("Author: " + user1 + " lName1"), "The user who created the topic is User1.");
-        assertTrue(topicViewPage.getTopicPublished().contains("Last reply by: " + user2 + " lName2"), "The user who last replied the topic is User2.");
+        assertTrue(topicViewPage.getTopicPublished().contains("Author: " + user1.get().getFirstName() + " " + user1.get().getLastName()), "The user who created the topic is User1.");
+        assertTrue(topicViewPage.getTopicPublished().contains("Last reply by: " + user2.get().getFirstName() + " " + user2.get().getLastName()), "The user who last replied the topic is User2.");
         assertTrue(topicViewPage.getTopicPublished().contains("Last reply on: " + today), "Last reply was today.");
         assertEquals(topicViewPage.getTopicContent(), topicContent, "Content is displayed.");
         assertEquals(topicViewPage.getTopicReplies(), "(1)", "Topic has 1 reply.");
         assertEquals(topicViewPage.getTopicTags(), topicTag, "Tag is displayed.");
 
-        LOG.info("Expected Result: Information about the reply is displayed.");
-        assertEquals(topicViewPage.getReplyAuthor(topicReply), user2 + " lName2", user2 + " posted that reply.");
+        log.info("Expected Result: Information about the reply is displayed.");
+        assertEquals(topicViewPage.getReplyAuthor(topicReply), user2.get().getFirstName() + " " + user2.get().getLastName(), user2 + " posted that reply.");
         assertTrue(topicViewPage.getReplyDate(topicReply).contains(today), "Reply was posted today.");
         assertEquals(topicViewPage.getReplyNoReplies(topicReply), "(0)", "Reply has 0 replies.");
     }
