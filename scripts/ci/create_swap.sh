907612f6951d1678c9856f3c25dbb2ed587bd3c4
rrajoria <rahul.rajoria@globallogic.com>
2024-04-17 16:00:03 +0530

    Fix Test Cases

--- scripts/ci/create_swap.sh
+++ scripts/ci/create_swap.sh
@@ -4,12 +4,12 @@ set -vex
 SIZE_IN_GB="${1}"
 
 sudo free -m -t
+sudo swapoff -a
 sudo fallocate -l ${SIZE_IN_GB}G /swapfile
 sudo dd if=/dev/zero of=/swapfile bs=1024 count=$((${SIZE_IN_GB} * 1024 * 1024))
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
 sudo free -m -t
-sudo swapoff -a
 sudo swapon --show
 free -h
