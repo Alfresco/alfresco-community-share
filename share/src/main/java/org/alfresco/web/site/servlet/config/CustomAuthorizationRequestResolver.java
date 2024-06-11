f1d0f1038c08b3612ea821c9e42145f3249814b5
MohinishSah <88024811+MohinishSah@users.noreply.github.com>
2024-06-11 23:44:09 +0530

    Apps 2735 share OIDC compliance (#696)
    * Refresh Token conditional processing
    
    * incorporating code changes for logout and refresh token
    
    * fixed redirect_uri issue
    
    * fixed logout_uri logic
    
    * Validate ID and Access Token, Audience Validator, Scope Inclusion, Principal Attribute Default Value
    
    * fixed logout_uri logic
    
    * fixed Logout issue
    
    * fixed Logout issue
    
    * Revert "fixed Logout issue"
    
    This reverts commit d1fdb42dd98c5c1170db1e66bff36cb5e885c432.
    
    * fixed Logout issue
    
    * Remove the logging via System out
    
    * fixed Logout issue for cognito
    
    * fixed Logout issue
    
    * Adding Junit for AIMSLogoutHandler (#695)
    
    Co-authored-by: Pardhu Kota <Kota.Pardhu@hyland.com>
    
    * adding Unit Test case
    
    * adding Unit Test case
    
    * Incorporated review comments
    
    * code cleanup
    
    * Formatting the Changes
    
    ---------
    
    Co-authored-by: kshah <kavit.shah@globallogic.com>
    Co-authored-by: Mohinish Sah <Mohinish.Sah@contractors.onbase.com>
    Co-authored-by: Pardhu Kota <Kota.Pardhu@hyland.com>

--- share/src/main/java/org/alfresco/web/site/servlet/config/CustomAuthorizationRequestResolver.java
+++ share/src/main/java/org/alfresco/web/site/servlet/config/CustomAuthorizationRequestResolver.java
@@ -1,42 +1,75 @@
 package org.alfresco.web.site.servlet.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+
 import jakarta.servlet.http.HttpServletRequest;
 
-public class CustomAuthorizationRequestResolver implements OAuth2AuthorizationRequestResolver {
+public class CustomAuthorizationRequestResolver implements OAuth2AuthorizationRequestResolver
+{
 
     private OAuth2AuthorizationRequestResolver defaultResolver;
+    private AIMSConfig aimsConfig;
+    private final static String AUDIENCE="audience";
 
-    public CustomAuthorizationRequestResolver(ClientRegistrationRepository repo, String authorizationRequestBaseUri){
+    public CustomAuthorizationRequestResolver(ClientRegistrationRepository repo, String authorizationRequestBaseUri,
+                                              AIMSConfig aimsConfig)
+    {
         defaultResolver = new DefaultOAuth2AuthorizationRequestResolver(repo, authorizationRequestBaseUri);
+        this.aimsConfig = aimsConfig;
     }
 
-
     @Override
-    public OAuth2AuthorizationRequest resolve(HttpServletRequest httpServletRequest) {
+    public OAuth2AuthorizationRequest resolve(HttpServletRequest httpServletRequest)
+    {
         OAuth2AuthorizationRequest oAuth2AuthorizationRequest = defaultResolver.resolve(httpServletRequest);
-        if(oAuth2AuthorizationRequest != null) {
+        if (oAuth2AuthorizationRequest != null)
+        {
             oAuth2AuthorizationRequest = customizeAuthorizationRequest(oAuth2AuthorizationRequest, httpServletRequest);
         }
         return oAuth2AuthorizationRequest;
     }
 
     @Override
-    public OAuth2AuthorizationRequest resolve(HttpServletRequest httpServletRequest, String clientRegistrationId) {
-        OAuth2AuthorizationRequest oAuth2AuthorizationRequest = defaultResolver.resolve(httpServletRequest, clientRegistrationId);
-        if(oAuth2AuthorizationRequest != null){
+    public OAuth2AuthorizationRequest resolve(HttpServletRequest httpServletRequest, String clientRegistrationId)
+    {
+        OAuth2AuthorizationRequest oAuth2AuthorizationRequest =
+            defaultResolver.resolve(httpServletRequest, clientRegistrationId);
+        if (oAuth2AuthorizationRequest != null)
+        {
             oAuth2AuthorizationRequest = customizeAuthorizationRequest(oAuth2AuthorizationRequest, httpServletRequest);
         }
         return oAuth2AuthorizationRequest;
     }
 
-    private OAuth2AuthorizationRequest customizeAuthorizationRequest(OAuth2AuthorizationRequest req, HttpServletRequest request) {
-        return OAuth2AuthorizationRequest
-            .from(req)
-            .redirectUri(String.valueOf(request.getRequestURL()))
-            .build();
+    private OAuth2AuthorizationRequest customizeAuthorizationRequest(OAuth2AuthorizationRequest authorizationRequest,
+                                                                     HttpServletRequest request)
+    {
+        Map<String, Object> extraParams = new HashMap<>();
+        String requestURI =
+            this.aimsConfig.getRedirectURI() != null ? this.aimsConfig.getRedirectURI() : String.valueOf(
+                request.getRequestURL());
+
+        if (this.aimsConfig.getAudience() != null)
+        {
+            extraParams.put(AUDIENCE, this.aimsConfig.getAudience());
+
+            return OAuth2AuthorizationRequest.from(authorizationRequest)
+                .redirectUri(requestURI)
+                .additionalParameters(extraParams)
+                .build();
+        }
+        else
+        {
+            return OAuth2AuthorizationRequest.from(authorizationRequest)
+                .redirectUri(requestURI)
+                .build();
+        }
     }
 }
+
