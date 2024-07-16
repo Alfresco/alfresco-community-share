1359a0c5d98c2a5e97ac20b6d12bc506b537835c
Domenico Sibilio <domenicosibilio@gmail.com>
2024-07-16 11:34:12 +0200

    ACS-8401 Migrate to Docker Compose v2 (#766)
    Co-authored-by: Piyush Joshi <89912489+pjoshi31@users.noreply.github.com>

--- amps/ags/ci/scripts/dockerLimitMemory.sh
+++ amps/ags/ci/scripts/dockerLimitMemory.sh
@@ -2,13 +2,13 @@
 set -x
 
 #stop not needed containers
-docker stop $(docker ps -a | grep '_zeppelin_' | awk '{print $1}')
-docker stop $(docker ps -a | grep '_sync-service_' | awk '{print $1}')
+docker stop $(docker ps -a | grep '\-zeppelin-' | awk '{print $1}')
+docker stop $(docker ps -a | grep '\-sync-service-' | awk '{print $1}')
 
-shareContainerId=$(docker ps -a | grep '_share_' | awk '{print $1}')
+shareContainerId=$(docker ps -a | grep '\-share-' | awk '{print $1}')
 if [ -n "$shareContainerId" ]; then
-      docker stop $(docker ps -a | grep '_transform-router_' | awk '{print $1}')
-      docker stop $(docker ps -a | grep '_shared-file-store_' | awk '{print $1}')
+      docker stop $(docker ps -a | grep '\-transform-router-' | awk '{print $1}')
+      docker stop $(docker ps -a | grep '\-shared-file-store-' | awk '{print $1}')
 fi
 
 # Display containers resources usage
