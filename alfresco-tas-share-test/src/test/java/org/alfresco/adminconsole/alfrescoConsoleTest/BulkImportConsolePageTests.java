162bd5826bd7b242c2247b59b484a67689d508c7
rajsharma <Rajat.Sharma@contractors.onbase.com>
2024-08-08 12:10:42 +0530

    Testcases refactored for APPS-2961

--- alfresco-tas-share-test/src/test/java/org/alfresco/adminconsole/alfrescoConsoleTest/BulkImportConsolePageTests.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/adminconsole/alfrescoConsoleTest/BulkImportConsolePageTests.java
@@ -1,30 +1,31 @@
 package org.alfresco.adminconsole.alfrescoConsoleTest;
 
-import org.alfresco.po.adminconsole.consoles.BulkImportConsolePage;
-import org.alfresco.adminconsole.ContextAwareWebAdminConsoleTest;
+import lombok.extern.slf4j.Slf4j;
+import org.alfresco.po.adminconsole.consoles.BulkImportConsolePages;
+import org.alfresco.share.BaseTest;
 import org.alfresco.utility.model.TestGroup;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.Test;
 
 /**
  * Created by Mirela Tifui on 11/16/2017.
  */
-public class BulkImportConsolePageTests extends ContextAwareWebAdminConsoleTest
+@Slf4j
+public class BulkImportConsolePageTests extends BaseTest
 {
-    @Autowired
-    private BulkImportConsolePage bulkImportConsolePage;
+    private BulkImportConsolePages bulkImportConsolePages;
 
     @Test (groups = { TestGroup.SHARE, "AlfrescoConsoles", "Acceptance" })
     public void accessBulkImportPage()
     {
-        LOG.info("Step 1: Access Bulk Import Console page");
-        bulkImportConsolePage.navigate()
-            .assertPageTitleIs("Bulk Filesystem In-place Import Tool")
-            .assertBatchSizeInputIsDisplayed()
-            .assertContentStoreInputIsDisplayed()
-            .assertImportDirectoryPathInputIsDisplayed()
-            .assertTargetPathInputIsDisplayed()
-            .assertNumThreadsInputIsDisplayed()
-            .assertSubmitButtonIsDisplayed();
+        bulkImportConsolePages = new BulkImportConsolePages(webDriver);
+        log.info("Step 1: Access Bulk Import Console page");
+        bulkImportConsolePages.navigate();
+        bulkImportConsolePages.assertPageTitleIs("Bulk Filesystem In-place Import Tool");
+        bulkImportConsolePages.assertBatchSizeInputIsDisplayed();
+        bulkImportConsolePages.assertContentStoreInputIsDisplayed();
+        bulkImportConsolePages.assertImportDirectoryPathInputIsDisplayed();
+        bulkImportConsolePages.assertTargetPathInputIsDisplayed();
+        bulkImportConsolePages.assertNumThreadsInputIsDisplayed();
+        bulkImportConsolePages.assertSubmitButtonIsDisplayed();
     }
 }
