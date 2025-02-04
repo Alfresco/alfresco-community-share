0568b3691ab1def5deaa06aea8022c2879327424
omkar.yadav <omkar.yadav@globallogic.com>
2025-02-04 17:30:26 +0530

    updated cleanup.sh script

--- scripts/ci/cleanup.sh
+++ scripts/ci/cleanup.sh
@@ -3,20 +3,30 @@
 echo "=========================== Starting Cleanup Script ==========================="
 PS4="\[\e[35m\]+ \[\e[m\]"
 set -vx
+set -e
 pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
+if [[ -z "$S3_BUCKET_NAME" || -z "$S3_BUCKET2_NAME" ]]; then
+    echo "Error: S3_BUCKET_NAME or S3_BUCKET2_NAME is not set."
+    exit 1
+fi
 
-# Stop and remove the containers
-docker ps -a -q | xargs -l -r docker stop
-docker ps -a -q | xargs -l -r docker rm
+if aws s3 ls "s3://${S3_BUCKET_NAME}" &>/dev/null; then
+    echo "Bucket '${S3_BUCKET_NAME}' exists. Deleting..."
+    aws s3 rb "s3://${S3_BUCKET_NAME}" --force || true
+    echo "Bucket '${S3_BUCKET_NAME}' deleted."
+else
+    echo "Bucket '${S3_BUCKET_NAME}' does not exist."
+fi
 
-pip install awscli
-printf "${AGS_AWS_ACCESS_KEY_ID}\n${AGS_AWS_SECRET_ACCESS_KEY}\n\n\n" | aws configure
+if aws s3 ls "s3://${S3_BUCKET2_NAME}" &>/dev/null; then
+    echo "Bucket '${S3_BUCKET2_NAME}' exists. Deleting..."
+    aws s3 rb "s3://${S3_BUCKET2_NAME}" --force || true
+    echo "Bucket '${S3_BUCKET_NAME}' deleted."
+else
+    echo "Bucket '${S3_BUCKET2_NAME}' does not exist."
+fi
 
-echo ${S3_BUCKET_NAME}
-
-aws s3 ls | awk '{print $3}' | grep "^${S3_BUCKET_NAME}" | xargs -l -r -I{} aws s3 rb "s3://{}" --force
-aws s3 ls | awk '{print $3}' | grep "^${S3_BUCKET2_NAME}" | xargs -l -r -I{} aws s3 rb "s3://{}" --force
 
 popd
 set +vx
