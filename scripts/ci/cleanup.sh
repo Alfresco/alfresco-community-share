abf1aa8d70ab16c889dfcb3490adc7e2c3060e86
omkar.yadav <omkar.yadav@globallogic.com>
2025-02-05 11:03:02 +0530

    added prefix to delete s3 bucket

--- scripts/ci/cleanup.sh
+++ scripts/ci/cleanup.sh
@@ -6,26 +6,17 @@ set -vx
 set -e
 pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
-if [[ -z "$S3_BUCKET_NAME" || -z "$S3_BUCKET2_NAME" ]]; then
-    echo "Error: S3_BUCKET_NAME or S3_BUCKET2_NAME is not set."
+if [[ -z "$S3_BUCKET_PREFIX" ]]; then
+    echo "Error: S3_BUCKET_PREFIX is not set."
     exit 1
 fi
 
-if aws s3 ls "s3://${S3_BUCKET_NAME}" &>/dev/null; then
-    echo "Bucket '${S3_BUCKET_NAME}' exists. Deleting..."
-    aws s3 rb "s3://${S3_BUCKET_NAME}" --force || true
-    echo "Bucket '${S3_BUCKET_NAME}' deleted."
-else
-    echo "Bucket '${S3_BUCKET_NAME}' does not exist."
-fi
-
-if aws s3 ls "s3://${S3_BUCKET2_NAME}" &>/dev/null; then
-    echo "Bucket '${S3_BUCKET2_NAME}' exists. Deleting..."
-    aws s3 rb "s3://${S3_BUCKET2_NAME}" --force || true
-    echo "Bucket '${S3_BUCKET2_NAME}' deleted."
-else
-    echo "Bucket '${S3_BUCKET2_NAME}' does not exist."
-fi
+aws s3api list-buckets --query "Buckets[?starts_with(Name, '${S3_BUCKET_PREFIX}')].Name" --output text | tr '\t' '\n' | while read bucket; do
+    if [[ -n "$bucket" ]]; then
+        echo "Deleting bucket: $bucket"
+        aws s3 rb "s3://$bucket" --force || true
+    fi
+done
 
 
 popd
