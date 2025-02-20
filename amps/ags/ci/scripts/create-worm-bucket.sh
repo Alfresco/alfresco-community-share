830179f7fb45aea663e5f6dbfbe0fec1c37398a4
anandh-prabhu <anandh.prabhu@hyland.com>
2025-02-20 14:47:57 +0530

    [ags] added script to delete S3 bucket along with header changes

--- amps/ags/ci/scripts/create-worm-bucket.sh
+++ amps/ags/ci/scripts/create-worm-bucket.sh
@@ -5,23 +5,20 @@ PS4="\[\e[35m\]+ \[\e[m\]"
 set -vex
 pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
-unset AWS_ACCESS_KEY_ID
-unset AWS_SECRET_ACCESS_KEY
-pip3 install awscli
-printf "${AGS_AWS_ACCESS_KEY_ID}\n${AGS_AWS_SECRET_ACCESS_KEY}\n\n\n" | aws configure
-
 if aws s3 ls | awk '{print $3}' | grep -q "^${S3_BUCKET2_NAME}$" ; then
   echo "Bucket ${S3_BUCKET2_NAME} already exists"
   exit 0
 fi
 
-aws s3api create-bucket --bucket "${S3_BUCKET2_NAME}" --region ${S3_BUCKET_REGION} --object-lock-enabled-for-bucket
-aws s3api put-object-lock-configuration \
-    --bucket "${S3_BUCKET2_NAME}" \
-    --object-lock-configuration 'ObjectLockEnabled=Enabled,Rule={DefaultRetention={Mode=COMPLIANCE,Days=1}}'
+if [[ "$S3_BUCKET2_NAME" == ${S3_BUCKET_PREFIX}* && ! -z "S3_BUCKET_PREFIX" ]]; then echo "creating bucket ${S3_BUCKET2_NAME}"
+  aws s3api create-bucket --bucket "${S3_BUCKET2_NAME}" --region ${S3_BUCKET_REGION} --object-lock-enabled-for-bucket
+  aws s3api put-object-lock-configuration \
+      --bucket "${S3_BUCKET2_NAME}" \
+      --object-lock-configuration 'ObjectLockEnabled=Enabled,Rule={DefaultRetention={Mode=COMPLIANCE,Days=1}}'
 
-aws s3api put-bucket-tagging --bucket "${S3_BUCKET2_NAME}" \
-   --tagging="TagSet=[{Key=toDeleteAfterTests,Value=true}]"
+ aws s3api put-bucket-tagging --bucket "${S3_BUCKET2_NAME}" \
+      --tagging="TagSet=[{Key=toDeleteAfterTests,Value=true}]"
+ fi
 
 popd
 set +vex
