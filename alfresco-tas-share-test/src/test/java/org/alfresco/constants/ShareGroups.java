98ee10269dae5c3d29018b0219d46833eafe0c20
snasim <shazia.nasim@hyland.com>
2026-01-09 14:54:35 +0530

    adding 2 more groups

--- alfresco-tas-share-test/src/test/java/org/alfresco/constants/ShareGroups.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/constants/ShareGroups.java
@@ -4,4 +4,6 @@ public interface ShareGroups {
     String TOBEFIXED = "tobefixed";
     String BUG = "bug";
     String SHARE_PRIORITY_1 = "SharePriority1";
+    String MISSING_JIRA = "MissingJira";
+    String MISSING_STEPS =" MissingSteps";
 }
