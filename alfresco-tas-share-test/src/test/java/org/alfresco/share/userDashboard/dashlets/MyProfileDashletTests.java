af20234e3e8c1916c878187abc60ab8284655f83
joyjit-hyland <joyjit.chandra@hyland.com>
2026-01-12 15:19:43 +0530

    User & Access Management

--- alfresco-tas-share-test/src/test/java/org/alfresco/share/userDashboard/dashlets/MyProfileDashletTests.java
+++ alfresco-tas-share-test/src/test/java/org/alfresco/share/userDashboard/dashlets/MyProfileDashletTests.java
@@ -1,9 +1,11 @@
 package org.alfresco.share.userDashboard.dashlets;
 
+import org.alfresco.constants.ShareGroups;
 import org.alfresco.dataprep.DashboardCustomization;
 import org.alfresco.po.enums.DashletHelpIcon;
 import org.alfresco.po.share.dashlet.MyProfileDashlet;
 import org.alfresco.po.share.user.profile.EditUserProfilePage;
+import org.alfresco.test.AlfrescoTest;
 import org.alfresco.testrail.TestRail;
 import org.alfresco.utility.data.RandomData;
 import org.alfresco.utility.model.TestGroup;
@@ -33,7 +35,8 @@ public class MyProfileDashletTests extends AbstractUserDashboardDashletsTests
     }
 
     @TestRail (id = "C2141")
-    @Test (groups = { TestGroup.REGRESSION, TestGroup.USER_DASHBOARD })
+    @AlfrescoTest(jira = "XAT-8228")
+    @Test (groups = { TestGroup.REGRESSION, TestGroup.USER_DASHBOARD, ShareGroups.SHARE_PRIORITY_1})
     public void checkMyProfileDashlet()
     {
         String jobTitle = String.format("jobTitle%s", RandomData.getRandomAlphanumeric());
